9bc75efd6d07df3c70fc5843025cc771a6bfe687
Nami Sunami <nsunami@pm.me>
2024-08-07 20:25:25 +0200

    Create AddJobProfile Modal

--- web/src/components/AddJobProfileModal/AddJobProfileModal.tsx
+++ web/src/components/AddJobProfileModal/AddJobProfileModal.tsx
@@ -1,3 +1,7 @@
+import { MutableRefObject } from 'react'
+
+import { X } from 'lucide-react'
+
 import {
   Form,
   Submit,
@@ -30,7 +34,11 @@ const CREATE_JOB_PROFILE = gql`
   }
 `
 
-const AddJobProfileModal = () => {
+const AddJobProfileModal = ({
+  dialogRef,
+}: {
+  dialogRef: MutableRefObject<HTMLDialogElement>
+}) => {
   const [create, { loading, error }] = useMutation(CREATE_JOB_PROFILE, {
     onCompleted: () => {
       toast.success('Success')
@@ -38,101 +46,181 @@ const AddJobProfileModal = () => {
   })
 
   function onSubmit(data) {
-    console.log(data)
     create({ variables: { input: data } })
   }
   return (
-    <div className="max-w-xl bg-alluca-primary text-alluca-gray">
+    <div className="max-w-xl rounded-xl bg-black p-2 text-alluca-gray">
+      <X
+        className="absolute right-1 top-1 text-white hover:cursor-pointer"
+        onClick={() => dialogRef.current.close()}
+      />
       <Toaster />
       <Form
         onSubmit={onSubmit}
-        className="flex flex-col"
+        className="flex flex-col gap-1"
         config={{ mode: 'onBlur' }}
       >
         <FormError error={error} wrapperClassName="bg-red-400" />
-        <Label name="name" errorClassName="text-red-500">
-          Functienaam
-        </Label>
-        <TextField
-          name="name"
-          errorClassName="border-red-500"
-          validation={{
-            required: {
-              value: true,
-              message: 'Functienaam is verplicht.',
-            },
-          }}
-        />
-        <FieldError name="name" className="text-red-600" />
+        <h2 className="font-bold text-accent">Functieprofielen aanmaken</h2>
+        <span>
+          <Label
+            name="name"
+            className="mr-2"
+            errorClassName="mr-2 text-yellow-500"
+          >
+            Functienaam
+          </Label>
+          <TextField
+            name="name"
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+            validation={{
+              required: {
+                value: true,
+                message: 'Functienaam is verplicht',
+              },
+            }}
+          />
+        </span>
+        <FieldError name="name" className="text-xs text-yellow-500" />
 
-        <Label name="qualityNeeded" errorClassName="bg-red-200">
-          Gewenste kwaliteit
-        </Label>
-        <NumberField
-          name="qualityNeeded"
-          validation={{ required: true }}
-          min={1}
-          max={5}
-        />
-
-        <Label name="yearsOfExp">Aantal jaar werkervaring</Label>
-        <NumberField
-          name="yearsOfExp"
-          min={0}
-          validation={{ required: true, valueAsNumber: true }}
-        />
-        <FieldError name="yearsOfExp" className="text-red-600" />
+        <span>
+          <Label name="qualityNeeded" className="mr-2">
+            Gewenste kwaliteit
+          </Label>
+          <NumberField
+            name="qualityNeeded"
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+            validation={{
+              required: {
+                value: true,
+                message: 'Gewenste kwaliteit is verplicht',
+              },
+            }}
+            min={1}
+            max={5}
+          />
+        </span>
+        <FieldError name="qualityNeeded" className="text-xs text-yellow-500" />
+        <div>
+          <Label name="yearsOfExp" className="mr-2">
+            Aantal jaar werkervaring
+          </Label>
+          <NumberField
+            name="yearsOfExp"
+            min={0}
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+            validation={{
+              required: {
+                value: true,
+                message: 'Aantal jaar werkervaring is verplicht',
+              },
+              valueAsNumber: true,
+            }}
+          />
+        </div>
+        <FieldError name="yearsOfExp" className="text-xs text-yellow-500" />
 
         {/* <Label name="certificates">Certificaten</Label>
         <TextField name="certificates" /> */}
 
         <fieldset>
           <legend>Salaris indicatie</legend>
-          <Label name="hourlyWageMin" errorClassName="error">
-            Van
-          </Label>
+          <span>€</span>
           <NumberField
             name="hourlyWageMin"
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
             min={0}
-            validation={{ required: true }}
+            validation={{
+              required: {
+                value: true,
+                message: 'Salaris indicatie (min) is verplicht',
+              },
+            }}
           />
-          <FieldError name="hourlyWageMin" className="text-red-600" />
-
-          <Label name="hourlyWageMin" errorClassName="error">
-            Tot
-          </Label>
+          —<span>€</span>
           <NumberField
             name="hourlyWageMax"
             min={0}
-            validation={{ required: true }}
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+            validation={{
+              required: {
+                value: true,
+                message: 'Salaris indicatie (max) is verplicht',
+              },
+            }}
           />
-          <FieldError name="hourlyWageMax" className="text-red-600" />
         </fieldset>
+        <FieldError name="hourlyWageMin" className="text-yellow-500" />
+        <FieldError name="hourlyWageMax" className="text-yellow-500" />
 
-        <Label name="maxTravelDistance">Maximale reisafstand</Label>
-        <NumberField name="maxTravelDistance" min={0} />
+        <span>
+          <Label name="maxTravelDistance" className="mr-2">
+            Maximale reisafstand
+          </Label>
+          <NumberField
+            name="maxTravelDistance"
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+            min={0}
+          />
+        </span>
 
         <fieldset>
-          <Label name="isTravelReimbursed">Reiskosten vergoeding</Label>
+          <Label name="isTravelReimbursed" className="mr-2">
+            Reiskosten vergoeding
+          </Label>
           <CheckboxField name="isTravelReimbursed" />
         </fieldset>
 
         <fieldset>
-          <Label name="isCarAvailable">Auto beschikbaar</Label>
+          <Label name="isCarAvailable" className="mr-2">
+            Auto beschikbaar
+          </Label>
           <CheckboxField name="isCarAvailable" />
         </fieldset>
 
-        <Label name="kmAllowance">Kilometervergoeding</Label>
-        <NumberField name="kmAllowance" min={0} />
+        <span>
+          <Label name="kmAllowance" className="mr-2">
+            Kilometervergoeding
+          </Label>
+          <NumberField
+            name="kmAllowance"
+            min={0}
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+          />
+        </span>
 
-        <Label name="totalBudgetPerHour">Budget bruto per uur</Label>
-        <NumberField name="totalBudgetPerHour" min={0} />
+        <span>
+          <Label name="totalBudgetPerHour" className="mr-2">
+            Budget bruto per uur
+          </Label>
+          <NumberField
+            name="totalBudgetPerHour"
+            min={0}
+            className="rounded-md border border-accent bg-black px-1 text-white"
+            errorClassName="bg-black border-yellow-500 border-2"
+          />
+        </span>
 
         <Label name="comment">
           Eventueel standaardbericht voor uitzendbureaus
         </Label>
-        <TextAreaField name="comment" />
-        <Submit disabled={loading} className="hover:cursor-pointer">
+        <TextAreaField
+          name="comment"
+          className="rounded-md border border-accent bg-black px-1 text-white"
+          errorClassName="bg-black border-yellow-500 border-2"
+        />
+
+        <Submit
+          disabled={loading}
+          className="bg-accent text-white hover:cursor-pointer"
+        >
           Aanmaken
         </Submit>
       </Form>
