35d5a279f804f0c02084506ec09b364f322a9627
Nami Sunami <nsunami@pm.me>
2024-08-07 20:25:34 +0200

    Update JobProfiles page with the modal

--- web/src/pages/JobProfilesPage/JobProfilesPage.tsx
+++ web/src/pages/JobProfilesPage/JobProfilesPage.tsx
@@ -1,20 +1,31 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useRef } from 'react'
+
+import { CirclePlus } from 'lucide-react'
+
 import { Metadata } from '@redwoodjs/web'
 
+import AddJobProfileModal from 'src/components/AddJobProfileModal/AddJobProfileModal'
 const JobProfilesPage = () => {
+  const addJobProfileRef = useRef<HTMLDialogElement>(null)
   return (
     <>
       <Metadata title="JobProfiles" description="JobProfiles page" />
 
-      <h1>JobProfilesPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/JobProfilesPage/JobProfilesPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>jobProfiles</code>, link to me with `
-        <Link to={routes.jobProfiles()}>JobProfiles</Link>`
-      </p>
+      <h1 className="font-bold text-white">Functieprofielen</h1>
+      <div className="flex justify-center">
+        <button
+          onClick={() => addJobProfileRef.current.showModal()}
+          className="font-bold"
+        >
+          <div className="flex">
+            <CirclePlus />
+            Aanmaken
+          </div>
+        </button>
+        <dialog ref={addJobProfileRef} className="rounded-xl bg-black">
+          <AddJobProfileModal dialogRef={addJobProfileRef} />
+        </dialog>
+      </div>
     </>
   )
 }
