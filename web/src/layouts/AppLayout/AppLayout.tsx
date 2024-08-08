9432e8c841d9f8406d9c1376de2373c6c656b85e
jazellemaira <jazellemaira@pm.me>
2024-08-08 19:08:38 +0200

    Add readable classnames

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -26,14 +26,14 @@ const AppLayout = ({ children }: AppLayoutProps) => {
   return (
     <div className="flex min-h-screen flex-col bg-primary text-primary-foreground">
       <header>
-        <NavigationMenu className="mx-2 flex w-full flex-wrap items-center justify-between">
-          <NavigationMenuList className="mx-4 flex  items-center">
+        <NavigationMenu className="navigation-bar mx-2 flex w-full flex-wrap items-center justify-between">
+          <NavigationMenuList className="navigation-links-section mx-4 flex items-center">
             <NavigationMenuItem>
               <Link to="/">
                 <img
                   src={notextlogo}
                   alt="logo"
-                  className="my-4 max-w-10 rounded-full transition-all hover:brightness-150"
+                  className="my-4 mr-2 max-w-10 rounded-full transition-all hover:brightness-150"
                 />
               </Link>
             </NavigationMenuItem>
@@ -59,24 +59,24 @@ const AppLayout = ({ children }: AppLayoutProps) => {
               </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
-          <NavigationMenuList className="mx-4 flex items-center">
-            <Avatar className="mx-2">
+          <NavigationMenuList className="user-profile-section mx-4 flex items-center">
+            <Avatar className="user-avatar mx-2">
               <AvatarImage src={avatar} alt="avatar" />
               <AvatarFallback>UI</AvatarFallback>
             </Avatar>
-            <NavigationMenuItem>
+            <NavigationMenuItem className="user">
               <NavigationMenuTrigger className="mr-2">
                 Username
               </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="row-span-3 bg-card-foreground p-4 text-card md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <div className="flex flex-row items-center">
-                    <Avatar className="mx-2">
-                      <AvatarImage src={avatar} alt="avatar" />
-                      <AvatarFallback>UI</AvatarFallback>
-                    </Avatar>
-                    <span>Username</span>
-                  </div>
+              <NavigationMenuContent className="user-profile-menu row-span-3 bg-card-foreground p-4 text-card md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <div className="user-profile flex flex-row items-center">
+                  <Avatar className="user-avatar mx-2">
+                    <AvatarImage src={avatar} alt="avatar" />
+                    <AvatarFallback>UI</AvatarFallback>
+                  </Avatar>
+                  <span>Username</span>
+                </div>
+                <ul className="user-profile-menu-list">
                   <NavigationMenuItem>
                     <Link to="">
                       <NavigationMenuLink
