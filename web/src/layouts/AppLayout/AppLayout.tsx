2dea4971c1d5ecd1b90cd66a9f5238624ff7a8e4
jazellemaira <jazellemaira@pm.me>
2024-08-08 18:20:36 +0200

    Organize nav components

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -24,87 +24,89 @@ type AppLayoutProps = {
 const AppLayout = ({ children }: AppLayoutProps) => {
   return (
     <div className="flex min-h-screen flex-col bg-primary text-primary-foreground">
-      <NavigationMenu className="flex w-full flex-wrap items-center justify-between">
-        <NavigationMenuList className="mx-4 flex  items-center">
-          <NavigationMenuItem>
-            <Link to="/">
-              <img
-                src={notextlogo}
-                alt="logo"
-                className="my-4 max-w-10 rounded-full transition-all hover:brightness-150"
-              />
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/overview">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Overzicht
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/plan">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Plan
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link to="/job-profiles">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Functieprofielen
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-        <NavigationMenuList className="mx-4 flex items-center">
-          <Avatar className="mx-2">
-            <AvatarImage src={avatar} alt="avatar" />
-            <AvatarFallback>UI</AvatarFallback>
-          </Avatar>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>[User Info]</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="row-span-3 bg-card-foreground p-4 text-card md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <div className="flex flex-row items-center">
-                  <Avatar className="mx-2">
-                    <AvatarImage src={avatar} alt="avatar" />
-                    <AvatarFallback>UI</AvatarFallback>
-                  </Avatar>
-                  <span>Username</span>
-                </div>
-                <NavigationMenuItem>
-                  <Link to="">
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Profiel
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link to="">
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Settings
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link to="">
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Logout
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <header>
+        <NavigationMenu className="flex w-full flex-wrap items-center justify-between">
+          <NavigationMenuList className="mx-4 flex  items-center">
+            <NavigationMenuItem>
+              <Link to="/">
+                <img
+                  src={notextlogo}
+                  alt="logo"
+                  className="my-4 max-w-10 rounded-full transition-all hover:brightness-150"
+                />
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/overview">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Overzicht
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/plan">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Plan
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/job-profiles">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Functieprofielen
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+          <NavigationMenuList className="mx-4 flex items-center">
+            <Avatar className="mx-2">
+              <AvatarImage src={avatar} alt="avatar" />
+              <AvatarFallback>UI</AvatarFallback>
+            </Avatar>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Username</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="row-span-3 bg-card-foreground p-4 text-card md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                  <div className="flex flex-row items-center">
+                    <Avatar className="mx-2">
+                      <AvatarImage src={avatar} alt="avatar" />
+                      <AvatarFallback>UI</AvatarFallback>
+                    </Avatar>
+                    <span>Username</span>
+                  </div>
+                  <NavigationMenuItem>
+                    <Link to="">
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        Profiel
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <Link to="">
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        Settings
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <Link to="">
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        Logout
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
       <main className="flex-grow bg-transparent">{children}</main>
       <footer>[Footer]</footer>
     </div>
