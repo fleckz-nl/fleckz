eba3ad37de18523f5bbc77f9d8b4a355b27931dc
jazellemaira <jazellemaira@pm.me>
2024-08-08 16:33:36 +0200

    Fix navigation bar

--- web/src/layouts/AppLayout/AppLayout.tsx
+++ web/src/layouts/AppLayout/AppLayout.tsx
@@ -1,4 +1,19 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@radix-ui/react-navigation-menu'
+
 import { Link } from '@redwoodjs/router'
+
+import {
+  NavigationMenuContent,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from 'src/components/ui/navigation-menu'
+
+import notextlogo from './notextlogo.png'
 
 type AppLayoutProps = {
   children?: React.ReactNode
@@ -6,27 +21,48 @@ type AppLayoutProps = {
 
 const AppLayout = ({ children }: AppLayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col bg-alluca-primary text-alluca-gray">
-      <header className="flex flex-row gap-2">
-        <h1>
-          <Link to="/">Alluca</Link>
-        </h1>
-        <nav className="flex grow flex-row justify-between">
-          <ul className="flex flex-row gap-2">
-            <li>
-              <Link to="/overview">Overzicht</Link>
-            </li>
-            <li>
-              <Link to="/plan">Plan</Link>
-            </li>
-            <li>
-              <Link to="/job-profiles">Functieprofielen</Link>
-            </li>
-          </ul>
-          <div>[User Info]</div>
-        </nav>
-      </header>
-      <main className="flex-grow bg-alluca-primary">{children}</main>
+    <div className="flex min-h-screen flex-col bg-primary text-primary-foreground">
+      <NavigationMenu className="flex w-full flex-row items-center justify-between">
+        <NavigationMenuList className="mx-4 flex items-center">
+          <NavigationMenuItem>
+            <Link to="/">
+              <img
+                src={notextlogo}
+                alt="logo"
+                className=" mx-2 my-4 max-w-10 rounded-full transition-all hover:brightness-150"
+              />
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/overview">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Overzicht
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/plan">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Plan
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link to="/job-profiles">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Functieprofielen
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+        <NavigationMenuList className="mx-4 flex items-center">
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>[User Info]</NavigationMenuTrigger>
+            <NavigationMenuContent></NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <main className="flex-grow bg-transparent">{children}</main>
       <footer>[Footer]</footer>
     </div>
   )
