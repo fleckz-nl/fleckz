d7b65ff9cc3f11aa7700f4ea426753c792d0e8ac
jazellemaira <jazellemaira@pm.me>
2024-08-09 08:35:49 +0200

    Create add button

--- web/src/pages/JobProfilesPage/JobProfilesPage.tsx
+++ web/src/pages/JobProfilesPage/JobProfilesPage.tsx
@@ -1,10 +1,11 @@
 import { useRef } from 'react'
 
-import { CirclePlus } from 'lucide-react'
+import { CirclePlus, SortDesc } from 'lucide-react'
 
 import { Metadata } from '@redwoodjs/web'
 
 import AddJobProfileModal from 'src/components/AddJobProfileModal/AddJobProfileModal'
+import { Button } from 'src/components/ui/button'
 const JobProfilesPage = () => {
   const addJobProfileRef = useRef<HTMLDialogElement>(null)
   return (
@@ -12,15 +13,14 @@ const JobProfilesPage = () => {
       <Metadata title="JobProfiles" description="JobProfiles page" />
       <h1 className="ml-8 text-xl font-bold text-white/90">Functieprofielen</h1>
       <div className="flex justify-center">
-        <button
+        <Button
+          variant="default"
           onClick={() => addJobProfileRef.current.showModal()}
-          className="font-bold"
+          className="mt-8 flex gap-1 py-6 text-lg font-bold"
         >
-          <div className="flex">
-            <CirclePlus />
-            Aanmaken
-          </div>
-        </button>
+          <CirclePlus size={30} className="text-accent hover:text-accent/30" />
+          Aanmaken
+        </Button>
         <dialog ref={addJobProfileRef} className="rounded-xl bg-black">
           <AddJobProfileModal dialogRef={addJobProfileRef} />
         </dialog>
