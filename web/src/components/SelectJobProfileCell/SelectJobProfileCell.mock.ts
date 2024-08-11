527d82ce239c71c2f5bb6e48402f18d08bbf3028
Nami Sunami <nsunami@pm.me>
2024-08-11 14:49:09 +0200

    Create a cell for rendering a Combobox for selecting Job Profile

---
+++ web/src/components/SelectJobProfileCell/SelectJobProfileCell.mock.ts
@@ -0,0 +1,74 @@
+import { useForm } from '@redwoodjs/forms'
+
+// Define your own mock data here:
+export const standard = (/* vars, { ctx, req } */) => ({
+  jobProfiles: [
+    {
+      id: 'clzo7lgno00013b6g6xlz8ghu',
+      name: 'Softwareontwikkelaar',
+      hourlyWageMin: 35,
+      hourlyWageMax: 50,
+      yearsOfExp: 3,
+      qualityNeeded: 4,
+      maxTravelDistance: 30,
+      kmAllowance: 0.21,
+      isCarAvailable: false,
+      isTravelReimbursed: true,
+      totalBudgetPerHour: 52.1,
+      comment:
+        'Kennis van moderne programmeertalen zoals Python en Java is een vereiste.',
+    },
+    {
+      id: 'ckldxzr7e000001jy5e5r3n1v',
+      name: 'Salesmanager',
+      hourlyWageMin: 30,
+      hourlyWageMax: 55,
+      yearsOfExp: 4,
+      qualityNeeded: 4,
+      maxTravelDistance: 50,
+      kmAllowance: 0.25,
+      isCarAvailable: true,
+      isTravelReimbursed: true,
+      totalBudgetPerHour: 55,
+      comment:
+        'Sterke onderhandelingsvaardigheden en ervaring in B2B-verkoop is een must.',
+    },
+    {
+      id: 'ckldxzr7e000101jycp9l76vv',
+      name: 'HR Specialist',
+      hourlyWageMin: 28,
+      hourlyWageMax: 42,
+      yearsOfExp: 3,
+      qualityNeeded: 4,
+      maxTravelDistance: 25,
+      kmAllowance: 0.19,
+      isCarAvailable: false,
+      isTravelReimbursed: true,
+      totalBudgetPerHour: 42,
+      comment:
+        'Kennis van arbeidsrecht en ervaring met werving en selectie is vereist.',
+    },
+    {
+      id: 'ckldxzr7e000201jytllvxa45',
+      name: 'Klantenservicemedewerker',
+      hourlyWageMin: 18,
+      hourlyWageMax: 28,
+      yearsOfExp: 1,
+      qualityNeeded: 2,
+      maxTravelDistance: 15,
+      kmAllowance: 0.15,
+      isCarAvailable: false,
+      isTravelReimbursed: true,
+      totalBudgetPerHour: 28.15,
+      comment:
+        'Uitstekende communicatieve vaardigheden en klantgerichtheid zijn essentieel.',
+    },
+  ],
+  className: 'bg-red-400',
+  field: {
+    name: 'jobProfileId',
+    value: 'ckldxzr7e000001jy5e5r3n1v',
+  },
+  // TODO: Complete the mock data
+  //
+})
---
+++ web/src/components/SelectJobProfileCell/SelectJobProfileCell.tsx
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+
+import { Check, ChevronsUpDown } from 'lucide-react'
+import type { JobProfilesQuery, JobProfilesQueryVariables } from 'types/graphql'
+
+import { ControllerRenderProps, UseFormReturn } from '@redwoodjs/forms'
+import type {
+  CellSuccessProps,
+  CellFailureProps,
+  TypedDocumentNode,
+} from '@redwoodjs/web'
+
+import { cn } from 'src/lib/utils'
+
+import { Button } from '../ui/button'
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '../ui/command'
+import { FormControl } from '../ui/form'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
+
+export const QUERY: TypedDocumentNode<
+  JobProfilesQuery,
+  JobProfilesQueryVariables
+> = gql`
+  query JobProfilesQuery {
+    jobProfiles {
+      id
+      name
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }: CellFailureProps) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+type SuccessProps = {
+  className?: string
+  field: ControllerRenderProps
+  form: UseFormReturn
+}
+
+export const Success = ({
+  jobProfiles,
+  className,
+  form,
+  field,
+}: CellSuccessProps<JobProfilesQuery> & SuccessProps) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger
+          asChild
+          className={cn(className, 'w-full')}
+          disabled={field.disabled}
+          onBlur={field.onBlur}
+        >
+          <FormControl>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="justify-between"
+            >
+              {field.value
+                ? jobProfiles.find((profile) => profile.id === field.value)
+                    ?.name
+                : 'Selecteer functieprofiel...'}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </FormControl>
+        </PopoverTrigger>
+        <PopoverContent className="p-0">
+          <Command>
+            <CommandInput placeholder="Search framework..." />
+            <CommandList>
+              <CommandEmpty>No framework found.</CommandEmpty>
+              <CommandGroup>
+                {jobProfiles.map((profile) => (
+                  <CommandItem
+                    key={profile.id}
+                    value={profile.name}
+                    onSelect={(currentValue) => {
+                      const selectedJobId = jobProfiles.find(
+                        (p) => p.name === currentValue
+                      ).id
+
+                      form.setValue(
+                        field.name,
+                        currentValue === field.value ? '' : selectedJobId
+                      )
+                      setOpen(false)
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        'mr-2 h-4 w-4',
+                        field.value === profile.id ? 'opacity-100' : 'opacity-0'
+                      )}
+                    />
+                    {profile.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
+  )
+}
