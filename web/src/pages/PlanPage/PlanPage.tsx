f8da3ba7dd294397c41da3505a5e242ed22ac297
Nami Sunami <nsunami@pm.me>
2024-08-11 17:54:24 +0200

    Style the PlanPage

--- web/src/pages/PlanPage/PlanPage.tsx
+++ web/src/pages/PlanPage/PlanPage.tsx
@@ -6,13 +6,17 @@ const PlanPage = () => {
   return (
     <>
       <Metadata title="Plan" description="Plan page" />
-      <div>
-        <h1 className="text-xl font-bold text-white/90 lg:mb-12 lg:ml-36 lg:mt-6">
-          Wanneer wil je dat er iemand komt?
-        </h1>
-        <p>Klik in de agenda en plan het werk in</p>
+      <div className="mx-auto flex max-w-4xl flex-col justify-between">
+        <div>
+          <h1 className="text-xl font-bold text-white/90">
+            Wanneer wil je dat er iemand komt?
+          </h1>
+          <p>Klik in de agenda en plan het werk in</p>
+        </div>
+        <div className="mt-4 flex items-center justify-center">
+          <PlanWorkComponent />
+        </div>
       </div>
-      <PlanWorkComponent />
     </>
   )
 }
