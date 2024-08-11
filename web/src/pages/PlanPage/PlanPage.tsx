cb3bda3a144ed76617bdcb607f809624b7d51410
Nami Sunami <nsunami@pm.me>
2024-08-11 15:16:28 +0200

    Implement the plan work component in the PlanPage

--- web/src/pages/PlanPage/PlanPage.tsx
+++ web/src/pages/PlanPage/PlanPage.tsx
@@ -1,19 +1,18 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import PlanWorkComponent from 'src/components/PlanWorkComponent/PlanWorkComponent'
 
 const PlanPage = () => {
   return (
     <>
       <Metadata title="Plan" description="Plan page" />
-
-      <h1>PlanPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/PlanPage/PlanPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>plan</code>, link to me with `
-        <Link to={routes.plan()}>Plan</Link>`
-      </p>
+      <div>
+        <h1 className="text-xl font-bold text-white/90 lg:mb-12 lg:ml-36 lg:mt-6">
+          Wanneer wil je dat er iemand komt?
+        </h1>
+        <p>Klik in de agenda en plan het werk in</p>
+      </div>
+      <PlanWorkComponent />
     </>
   )
 }
