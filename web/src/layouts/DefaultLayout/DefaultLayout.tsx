150e4402a646f6fcca87a10fd5d530ef7bca06e2
jazellemaira <jazellemaira@pm.me>
2024-08-11 11:44:17 +0200

    Fix page layout

--- web/src/layouts/DefaultLayout/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,19 +1,24 @@
 import { Link } from '@redwoodjs/router'
 
+import logo from 'src/pages/HomePage/logo.png'
+
 type DefaultLayoutProps = {
   children?: React.ReactNode
 }
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col">
-      <header className="flex flex-row gap-2">
-        <h1>
-          <Link to="/">[Logo] Alluca</Link>
-        </h1>
+    <div className="flex min-h-screen flex-col bg-primary">
+      <header className="mt-10 flex flex-row items-center justify-center md:mt-24">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-20 rounded-full drop-shadow-sm transition-all hover:brightness-110"
+          />
+        </Link>
       </header>
-      <main className="flex-grow">{children}</main>
-      <footer>[Footer]</footer>
+      <main className="-mt-6 w-full max-w-sm flex-grow">{children}</main>
     </div>
   )
 }
