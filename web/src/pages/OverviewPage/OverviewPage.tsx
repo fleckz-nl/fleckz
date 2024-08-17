d6c88b96e5bd57a796d2511d3bf3c13e938129aa
Nami Sunami <nsunami@pm.me>
2024-08-17 18:49:18 +0200

    Implement WorkRequestsCell on OverviewPage

--- web/src/pages/OverviewPage/OverviewPage.tsx
+++ web/src/pages/OverviewPage/OverviewPage.tsx
@@ -1,19 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
+import WorkRequestsCell from 'src/components/WorkRequestsCell'
+
 const OverviewPage = () => {
   return (
     <>
       <Metadata title="Overview" description="Overview page" />
-
-      <h1>OverviewPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/OverviewPage/OverviewPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>overview</code>, link to me with `
-        <Link to={routes.overview()}>Overview</Link>`
-      </p>
+      <WorkRequestsCell />
     </>
   )
 }
