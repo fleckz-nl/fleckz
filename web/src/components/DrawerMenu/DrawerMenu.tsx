ca62295ca9b5c5685f8bec0d76093d3dbeb7e22c
Nami Sunami <nsunami@pm.me>
2024-08-18 16:10:22 +0200

    Remove rounding from top left

--- web/src/components/DrawerMenu/DrawerMenu.tsx
+++ web/src/components/DrawerMenu/DrawerMenu.tsx
@@ -27,7 +27,7 @@ const DrawerMenu = () => {
       <DrawerTrigger className="absolute">
         <HamburgerMenuIcon className="hover:pointer mx-2 block h-7 w-7 transition-colors hover:text-accent xs:hidden" />
       </DrawerTrigger>
-      <DrawerContent className="h-screen w-5/6 outline-none">
+      <DrawerContent className="h-screen w-5/6 rounded-tl-none outline-none">
         <DrawerHeader>
           <DrawerClose>
             <X />
