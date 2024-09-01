9a4faf9c009f60b2a50281c3492b648c2bab9766
jazellemaira <jazellemaira@pm.me>
2024-09-01 20:38:04 +0200

    Add data to storybook
    Co-authored-by: Nami Sunami <nsunami@pm.me>

--- web/src/components/RequestStatusCard/RequestStatusCard.stories.tsx
+++ web/src/components/RequestStatusCard/RequestStatusCard.stories.tsx
@@ -11,6 +11,7 @@
 // See https://storybook.js.org/docs/react/writing-stories/args.
 
 import type { Meta, StoryObj } from '@storybook/react'
+import type { WorkRequestsQuery } from 'types/graphql'
 
 import RequestStatusCard from './RequestStatusCard'
 
@@ -23,4 +24,53 @@ export default meta
 
 type Story = StoryObj<typeof RequestStatusCard>
 
-export const Primary: Story = {}
+const defaultRequest: WorkRequestsQuery['workRequests'][0] = {
+  __typename: 'WorkRequest',
+  id: 'test',
+  projectName: 'test project',
+  startDate: new Date().toDateString(),
+  endDate: new Date().toDateString(),
+  status: 'DRAFT',
+  numWorkers: 1,
+  location: {
+    __typename: 'Address',
+    id: 'xxx',
+    street: 'xxx',
+    houseNumber: 'xxx',
+    city: 'xxxx',
+    province: 'xxx',
+    country: 'xxx',
+    postalCode: 'xxx',
+  },
+  jobProfile: {
+    __typename: 'JobProfile',
+    name: 'xxx',
+    hourlyWageMin: 4,
+    hourlyWageMax: 4,
+    qualityNeeded: 4,
+  },
+}
+
+export const Draft: Story = {
+  args: {
+    request: defaultRequest,
+  },
+}
+
+export const Submitted: Story = {
+  args: {
+    request: { ...defaultRequest, status: 'SUBMITTED' },
+  },
+}
+
+export const Confirmed: Story = {
+  args: {
+    request: { ...defaultRequest, status: 'CONFIRMED' },
+  },
+}
+
+export const Done: Story = {
+  args: {
+    request: { ...defaultRequest, status: 'DONE' },
+  },
+}
