a62ffb0e1b70844e12e61fe0023a3f054df02d1f
jazellemaira <jazellemaira@pm.me>
2024-09-04 19:45:17 +0200

    Edit badge component

--- web/src/components/ui/badge.tsx
+++ web/src/components/ui/badge.tsx
@@ -5,17 +5,18 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from 'src/lib/utils'
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold focus:outline-none transition-colors focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
         default:
           'border-transparent bg-primary/60 text-primary shadow hover:bg-primary/80 hover:text-muted/90 rounded-xl',
         secondary:
-          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80 rounded-xl',
+          'border-transparent hover:bg-accent/60 text-gray-600 bg-accent/30 rounded-xl hover:text-white',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80 rounded-xl',
-        outline: 'text-foreground',
+        outline:
+          'text-white/80 hover:bg-primary-foreground/30 rounded-xl hover:text-gray-600 bg-primary-foreground/50 border-transparent hover:border-primary/80',
       },
     },
     defaultVariants: {
