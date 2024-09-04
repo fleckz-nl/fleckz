49fa6c138b1c052538dc7593622d378efac7fe35
Nami Sunami <nsunami@pm.me>
2024-09-04 21:18:33 +0200

    Add mock data for temp agencies

--- scripts/seedFakeData.ts
+++ scripts/seedFakeData.ts
@@ -4,12 +4,14 @@ import { db } from 'api/src/lib/db'
 import { seedAddresses } from './utils/seedAddresses'
 import { seedCertificates } from './utils/seedCertificates'
 import { seedJobProfiles } from './utils/seedJobProfiles'
+import { seedTempAgencies } from './utils/seedTempAgencies'
 
 export default async () => {
   try {
     seedCertificates(db)
     seedJobProfiles(db)
     seedAddresses(db)
+    seedTempAgencies(db)
   } catch (error) {
     console.error(error)
   }
