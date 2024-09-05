b8e49c5ee990411c9ac821e40f6c370ca759269d
jazellemaira <jazellemaira@pm.me>
2024-09-05 15:53:27 +0200

    Wrap page content in a div

--- web/src/pages/SettingsPage/SettingsPage.tsx
+++ web/src/pages/SettingsPage/SettingsPage.tsx
@@ -5,15 +5,16 @@ const SettingsPage = () => {
   return (
     <>
       <Metadata title="Settings" description="Settings page" />
-
-      <h1>SettingsPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/SettingsPage/SettingsPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>settings</code>, link to me with `
-        <Link to={routes.settings()}>Settings</Link>`
-      </p>
+      <div className="mx-auto flex max-w-4xl flex-col">
+        <h1>SettingsPage</h1>
+        <p>
+          Find me in <code>./web/src/pages/SettingsPage/SettingsPage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>settings</code>, link to me with `
+          <Link to={routes.settings()}>Settings</Link>`
+        </p>
+      </div>
     </>
   )
 }
