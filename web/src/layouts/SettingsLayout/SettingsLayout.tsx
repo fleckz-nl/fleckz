218de5802d46e738d184f55a1a2a1e2455f97c12
jazellemaira <jazellemaira@pm.me>
2024-09-05 22:54:14 +0200

    Style base settings' layout

--- web/src/layouts/SettingsLayout/SettingsLayout.tsx
+++ web/src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -1,9 +1,20 @@
+import Nav from 'src/components/Nav/Nav'
+
 type SettingLayoutProps = {
   children?: React.ReactNode
 }
 
 const SettingLayout = ({ children }: SettingLayoutProps) => {
-  return <>{children}</>
+  return (
+    <>
+      <div className="flex min-h-screen flex-col bg-black text-primary-foreground">
+        <Nav />
+        <main className="max-w-full flex-grow flex-col break-words bg-transparent px-4 py-20">
+          {children}
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default SettingLayout
