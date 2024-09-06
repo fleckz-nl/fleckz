71f6a045c22f72c8f06b4c47efec8b432201a2ab
jazellemaira <jazellemaira@pm.me>
2024-09-06 19:12:59 +0200

    Center SettingsLayout content

--- web/src/layouts/SettingsLayout/SettingsLayout.tsx
+++ web/src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -7,7 +7,7 @@ type SettingLayoutProps = {
 const SettingLayout = ({ children }: SettingLayoutProps) => {
   return (
     <>
-      <div className="flex min-h-screen flex-col bg-black text-primary-foreground">
+      <div className="center min-h-screen flex-col bg-black text-primary-foreground">
         <Nav />
         <main className="max-w-full flex-grow flex-col break-words bg-transparent px-4 py-20">
           {children}
