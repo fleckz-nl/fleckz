7eb63d2eea3eaf826995f97a63f6d8fc594770ef
jazellemaira <jazellemaira@pm.me>
2024-09-06 22:30:36 +0200

    Style SettingsLayout

--- web/src/layouts/SettingsLayout/SettingsLayout.tsx
+++ web/src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -1,3 +1,4 @@
+import Aside from 'src/components/Aside/Aside'
 import Nav from 'src/components/Nav/Nav'
 
 type SettingLayoutProps = {
@@ -10,7 +11,21 @@ const SettingLayout = ({ children }: SettingLayoutProps) => {
       <div className="center min-h-screen flex-col bg-black text-primary-foreground">
         <Nav />
         <main className="max-w-full flex-grow flex-col break-words bg-transparent px-4 pt-20">
-          {children}
+          <div className="flex min-h-screen flex-col">
+            <div className="flex-1">
+              <div className="grid gap-4 px-2 xs:grid-cols-[140px_minmax(0,1fr)] xs:px-4 md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-8">
+                <Aside />
+                <main className="relative ml-10 bg-stone-800 py-6 xs:ml-8 md:ml-0 md:gap-8 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+                  <div className="center container mx-auto min-h-screen min-w-0">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Molestiae veniam, similique ad omnis perferendis, hic nulla
+                    adipisci, quisquam eligendi magni provident dolorem.
+                    {children}
+                  </div>
+                </main>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     </>
