e815d75d37478273972b3cfe38a4de012bfefebb
jazellemaira <jazellemaira@pm.me>
2024-09-07 19:49:05 +0200

    Tweak page containers

--- web/src/pages/ProfilePage/ProfilePage.tsx
+++ web/src/pages/ProfilePage/ProfilePage.tsx
@@ -26,8 +26,8 @@ const ProfilePage = () => {
   return (
     <>
       <Metadata title="Profile" description="Profile page" />
-      <div className="container max-w-3xl bg-black">
-        <div className="relative -top-7">
+      <div className="xs:container xs:max-w-3xl">
+        <div className="relative -top-7 -mb-5">
           <h1 className="text-xl font-semibold tracking-tight text-white">
             Uw Profiel
           </h1>
