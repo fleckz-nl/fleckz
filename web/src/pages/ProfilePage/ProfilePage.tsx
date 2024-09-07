8ec965c231f112311648f7ef008c44c7be8685c4
jazellemaira <jazellemaira@pm.me>
2024-09-07 16:36:03 +0200

    Add page header and description

--- web/src/pages/ProfilePage/ProfilePage.tsx
+++ web/src/pages/ProfilePage/ProfilePage.tsx
@@ -4,6 +4,14 @@ const ProfilePage = () => {
   return (
     <>
       <Metadata title="Profile" description="Profile page" />
+      <div className="container max-w-3xl bg-black">
+        <div className="relative -top-7">
+          <h1 className="text-xl font-semibold tracking-tight text-white">
+            Uw Profiel
+          </h1>
+          <p className="font-thin">Beheer uw profiel</p>
+        </div>
+      </div>
     </>
   )
 }
