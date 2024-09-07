597f1688ff2abd392ed9338eaa8238f54964e219
jazellemaira <jazellemaira@pm.me>
2024-09-07 13:00:55 +0200

    Replace a tags with link

---
+++ web/src/components/SideNav/SideNav.tsx
@@ -0,0 +1,25 @@
+import { Link, routes } from '@redwoodjs/router'
+            <li className="w-full py-1 pl-1 xs:pl-0">
+              <Link to={routes.profile()}>
+                <User className="size-5" />{' '}
+                <h4 className="hidden text-left xs:block">Profiel</h4>
+              </Link>
+            </li>
+            <li className="w-full py-1 pl-1 xs:pl-0">
+              <Link to={routes.profile()}>
+                <Building2 className="size-5" />{' '}
+                <h4 className="hidden text-left xs:block">Werkplekken</h4>
+              </Link>
+            </li>
+            <li className="w-full py-1 pl-1 xs:pl-0">
+              <Link to={routes.profile()}>
+                <CreditCard className="size-5" />{' '}
+                <h4 className="hidden text-left xs:block">Factuurgegevens</h4>
+              </Link>
+            </li>
+            <li className="w-full py-1 pl-1 xs:pl-0">
+              <Link to={routes.profile()}>
+                <Earth className="size-5" />{' '}
+                <h4 className="hidden text-left xs:block">Tijd en Locatie</h4>
+              </Link>
+            </li>
