7ccadf6cc90c4cd1512d2eb6d00ffe9f6d23fbee
jazellemaira <jazellemaira@pm.me>
2024-09-07 16:34:37 +0200

    Adjust layout

--- web/src/layouts/SettingsLayout/SettingsLayout.tsx
+++ web/src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -10,23 +10,16 @@ const SettingLayout = ({ children }: SettingLayoutProps) => {
     <>
       <div className="center min-h-screen flex-col bg-black text-primary-foreground">
         <Nav />
-        <main className="max-w-full flex-grow flex-col break-words bg-transparent px-4 pt-20">
-          <div className="flex min-h-screen flex-col">
-            <div className="flex-1">
-              <div className="grid gap-2 px-2 xs:grid-cols-[180px_minmax(0,1fr)] xs:gap-2 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-4">
-                <SideNav className="text-white/80" />
-                <main className="relative ml-10 bg-stone-800 py-6 xs:ml-2 md:gap-8 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
-                  <div className="center container mx-auto min-h-screen min-w-0">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Molestiae veniam, similique ad omnis perferendis, hic nulla
-                    adipisci, quisquam eligendi magni provident dolorem.
-                    {children}
-                  </div>
-                </main>
+        <section className="w-full flex-grow flex-col break-words bg-transparent px-4 pt-20">
+          <div className="container grid flex-1 gap-2 px-2 xs:grid-cols-[180px_minmax(0,1fr)] xs:gap-2 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-4">
+            <SideNav className="text-white/80" />
+            <main className="relative ml-10 py-6 xs:ml-2 xs:w-full md:gap-8 lg:py-8">
+              <div className="flex min-h-screen w-full bg-red-600">
+                {children}
               </div>
-            </div>
+            </main>
           </div>
-        </main>
+        </section>
       </div>
     </>
   )
