f0311876db8dc2723d7eb4caf739cf614f16ae19
jazellemaira <jazellemaira@pm.me>
2024-09-07 13:06:25 +0200

    Adjust style breakpoints

--- web/src/layouts/SettingsLayout/SettingsLayout.tsx
+++ web/src/layouts/SettingsLayout/SettingsLayout.tsx
@@ -13,9 +13,9 @@ const SettingLayout = ({ children }: SettingLayoutProps) => {
         <main className="max-w-full flex-grow flex-col break-words bg-transparent px-4 pt-20">
           <div className="flex min-h-screen flex-col">
             <div className="flex-1">
-              <div className="grid gap-4 px-2 xs:grid-cols-[140px_minmax(0,1fr)] xs:px-4 md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-8">
+              <div className="grid gap-2 px-2 xs:grid-cols-[180px_minmax(0,1fr)] xs:gap-2 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-4">
                 <SideNav className="text-white/80" />
-                <main className="relative ml-10 bg-stone-800 py-6 xs:ml-8 md:ml-0 md:gap-8 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
+                <main className="relative ml-10 bg-stone-800 py-6 xs:ml-2 md:gap-8 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
                   <div className="center container mx-auto min-h-screen min-w-0">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Molestiae veniam, similique ad omnis perferendis, hic nulla
