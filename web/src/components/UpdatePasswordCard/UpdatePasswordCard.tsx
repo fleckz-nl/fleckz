b89ea9658583e98289f9831c2f40af09feed89a3
Nami Sunami <nsunami@pm.me>
2024-09-08 21:12:46 +0200

    Generate UpdatePasswordCard Component

---
+++ web/src/components/UpdatePasswordCard/UpdatePasswordCard.tsx
@@ -0,0 +1,14 @@
+const UpdatePasswordCard = () => {
+  return (
+    <div>
+      <h2>{'UpdatePasswordCard'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/UpdatePasswordCard/UpdatePasswordCard.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default UpdatePasswordCard
