6d007bba34fc21ae9abccb431645a93f6a3b4fe1
jazellemaira <jazellemaira@pm.me>
2024-09-08 14:27:40 +0200

    Change structure of work request summary section.

--- web/src/components/WorkRequestCell/WorkRequestCell.tsx
+++ web/src/components/WorkRequestCell/WorkRequestCell.tsx
@@ -99,16 +99,29 @@ export const Success = ({
 }: CellSuccessProps<FindWorkRequestQuery, FindWorkRequestQueryVariables> &
   SuccessProps) => {
   return (
+    <div className="flex min-h-screen w-full flex-wrap justify-between">
+      <div className="flex w-full max-w-5xl flex-grow flex-wrap justify-center bg-white">
+        <div className="w-full bg-white pl-2 pt-4 text-xl font-bold hover:text-accent xs:mb-4 xs:pl-8">
+          <Link to={routes.overview()} className="flex items-center">
+            <ArrowLeft className="mr-1 inline" /> Overzicht
+          </Link>
+        </div>
+        <div className="container flex flex-col items-center gap-8 pt-4 xs:pt-0">
+          <div className="flex w-full flex-wrap items-center justify-between gap-4">
+            <section className="px-0 xs:mx-auto">
               <RequestStatusCard
                 request={workRequest}
                 className={cn('max-w-[340px]', className)}
               />
+              <div className="my-2 text-left text-sm xs:my-0">
+                <h3 className="font-semibold">Op verzoek van:</h3>
                 <p>Jos Janssen</p>
                 <p>16 August 2024 02:00 pm</p>
               </div>
             </section>
             <Separator className="xs:hidden" />
             <JobProfileDetailsTable jobProfile={workRequest.jobProfile} />
+          </div>
           <Separator className="xs:hidden" />
         </div>
       </div>
