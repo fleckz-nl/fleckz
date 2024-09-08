b7864b8ce3572f0d26f9fcb4c65c68c2a1a42572
Nami Sunami <nsunami@pm.me>
2024-09-08 18:46:33 +0200

    Use update avatar mutation

--- web/src/components/UpdateAvatar/UpdateAvatar.tsx
+++ web/src/components/UpdateAvatar/UpdateAvatar.tsx
@@ -1,21 +1,103 @@
-import { ImageUp, User } from 'lucide-react'
+import '@uploadcare/react-uploader/core.css'
+
+import { useRef, useState } from 'react'
+
+import {
+  FileUploaderRegular,
+  OutputCollectionState,
+  OutputCollectionStatus,
+  UploadCtxProvider,
+} from '@uploadcare/react-uploader'
+import {
+  ImageUp,
+  LoaderCircle,
+  MessageSquareWarningIcon,
+  User,
+} from 'lucide-react'
+
+import { FormError } from '@redwoodjs/forms'
+import { useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/dist/toast'
 
 import { useAuth } from 'src/auth'
 
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 
+const UPDATE_AVATAR_URL_GQL = gql`
+  mutation UpdateAvatarUrl($id: String!, $newAvatarUrl: String!) {
+    updateAvatarUrl(id: $id, newAvatarUrl: $newAvatarUrl) {
+      id
+    }
+  }
+`
+
 const UpdateAvatar = () => {
-  const { currentUser } = useAuth()
+  const { currentUser, reauthenticate } = useAuth()
+  const uploaderRef = useRef<InstanceType<UploadCtxProvider> | null>(null)
+
+  const [avatarUrl, setAvatarUrl] = useState(currentUser.avatarUrl)
+
+  const [updateAvatar, { loading, error }] = useMutation(
+    UPDATE_AVATAR_URL_GQL,
+    {
+      onCompleted: () => {
+        toast.success('Avatar bijgewerkt')
+      },
+    }
+  )
+
+  async function handleUploadcareDone(
+    e: OutputCollectionState<OutputCollectionStatus, 'maybe-has-group'>
+  ) {
+    const loadingToast = toast.loading('Laden...')
+    const avatarUrl = e.allEntries[0].cdnUrl
+    await updateAvatar({
+      variables: {
+        id: currentUser.id,
+        newAvatarUrl: avatarUrl,
+      },
+    })
+    await reauthenticate()
+    toast.dismiss(loadingToast)
+    setAvatarUrl(avatarUrl)
+  }
+
   return (
-    <div className="relative my-4">
-      <Avatar className="h-32 w-32">
-        <AvatarImage src={currentUser.avatarUrl} />
-        <AvatarFallback className="bg-secondary text-black">
-          <User className="size-28 stroke-1" />
-        </AvatarFallback>
-      </Avatar>
-      <ImageUp className="absolute bottom-2 right-[26] text-white shadow-sm shadow-accent" />
-    </div>
+    <>
+      <div className="relative my-4">
+        {error && (
+          <div className="flex h-fit items-center justify-center gap-2 bg-red-200 py-2 pl-2 text-red-600">
+            <MessageSquareWarningIcon />
+            <FormError error={error} />
+          </div>
+        )}
+        <button
+          onClick={() => {
+            uploaderRef.current.api.initFlow()
+          }}
+        >
+          <div className="absolute inset-0 z-20 flex items-center justify-center">
+            {loading && <LoaderCircle className="size-16 animate-spin" />}
+          </div>
+          <Avatar className="h-32 w-32">
+            <AvatarImage src={avatarUrl} />
+            <AvatarFallback className="bg-secondary text-black">
+              <User className="size-28 stroke-1" />
+            </AvatarFallback>
+          </Avatar>
+          <ImageUp className="absolute bottom-2 right-[26] text-white shadow-sm shadow-accent" />
+        </button>
+      </div>
+      <FileUploaderRegular
+        sourceList="local, url, camera"
+        classNameUploader="uc-dark"
+        pubkey={process.env.UPLOADCARE_PUBLIC_KEY}
+        apiRef={uploaderRef}
+        className="invisible"
+        onDoneClick={handleUploadcareDone}
+        multiple={false}
+      />
+    </>
   )
 }
 
