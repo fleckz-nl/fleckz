3d86a3e7d9fa8d01b011c44359ee167eeb6c4a64
jazellemaira <jazellemaira@pm.me>
2024-09-09 15:34:32 +0200

    Delete boilerplate content

--- web/src/components/WorkPlacesCard/WorkPlacesCard.tsx
+++ web/src/components/WorkPlacesCard/WorkPlacesCard.tsx
@@ -1,12 +1,5 @@
 const WorkPlacesCard = () => {
-  return (
-    <div>
-      <h2>{'WorkPlacesCard'}</h2>
-      <p>
-        {'Find me in ./web/src/components/WorkPlacesCard/WorkPlacesCard.tsx'}
-      </p>
-    </div>
-  )
+  return <></>
 }
 
 export default WorkPlacesCard
