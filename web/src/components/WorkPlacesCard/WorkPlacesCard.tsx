f92421d542e42a6fd1d97a7935177597c275429c
jazellemaira <jazellemaira@pm.me>
2024-09-09 16:15:05 +0200

    Create WorkPlacesCard layout

--- web/src/components/WorkPlacesCard/WorkPlacesCard.tsx
+++ web/src/components/WorkPlacesCard/WorkPlacesCard.tsx
@@ -1,5 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
+
 const WorkPlacesCard = () => {
-  return <></>
+  return (
+    <Card className="border-gray-600/40 bg-black text-white xs:max-w-full">
+      <CardHeader>
+        <CardTitle className="text-white">Werkplekken</CardTitle>
+        <CardDescription className="text-white/60">
+          Werk uw werkplekken bij.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2"></CardContent>
+    </Card>
+  )
 }
 
 export default WorkPlacesCard
