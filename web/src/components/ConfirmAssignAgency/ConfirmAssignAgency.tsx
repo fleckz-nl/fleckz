8e79153f2a72a91627fd2665ec1e3b4f2e7db9ba
Nami Sunami <nsunami@pm.me>
2024-09-09 21:38:49 +0200

    Add confirmation dialog when changing an agency

--- web/src/components/ConfirmAssignAgency/ConfirmAssignAgency.tsx
+++ web/src/components/ConfirmAssignAgency/ConfirmAssignAgency.tsx
@@ -1,13 +1,47 @@
-const ConfirmAssignAgency = () => {
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from 'src/components/ui/alert-dialog'
+
+type ConfirmAssignAgencyProps = {
+  open?: boolean
+  onOpenChange?: () => void
+  hideTrigger?: boolean
+  onConfirm: () => void
+  newAgency: string
+}
+
+const ConfirmAssignAgency = ({
+  open,
+  onOpenChange,
+  hideTrigger,
+  onConfirm,
+  newAgency,
+}: ConfirmAssignAgencyProps) => {
   return (
-    <div>
-      <h2>{'ConfirmAssignAgency'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/ConfirmAssignAgency/ConfirmAssignAgency.tsx'
-        }
-      </p>
-    </div>
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogTrigger hidden={hideTrigger}>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Bevestig toewijzing uitzendbureau</AlertDialogTitle>
+          <AlertDialogDescription>
+            Bevestig dat u het uitzendbureau wilt wijzigen naar{' '}
+            <span className="font-bold text-primary">{newAgency}</span>.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Annuleren</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Bevestigen</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
