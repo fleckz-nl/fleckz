a4a67dba49e9ec72d587f386bca84c40df3b8041
jazellemaira <jazellemaira@pm.me>
2024-09-09 13:41:27 +0200

    Add main header and description

--- web/src/pages/WorkPlacesPage/WorkPlacesPage.tsx
+++ web/src/pages/WorkPlacesPage/WorkPlacesPage.tsx
@@ -4,6 +4,14 @@ const WorkPlacesPage = () => {
   return (
     <>
       <Metadata title="WorkPlaces" description="WorkPlaces page" />
+      <div className="xs:container xs:max-w-3xl">
+        <div className="relative -top-7 -mb-5">
+          <h1 className="text-xl font-semibold tracking-tight text-white">
+            Uw Werkplekken
+          </h1>
+          <p>Beheer uw werkplekken</p>
+        </div>
+      </div>
     </>
   )
 }
