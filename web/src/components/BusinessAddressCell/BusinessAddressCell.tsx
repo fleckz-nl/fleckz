93882ca3aab79b9dfbc50e4105879376a48a6dfd
jazellemaira <jazellemaira@pm.me>
2024-09-09 22:25:06 +0200

    Adjust width and X button position

--- web/src/components/BusinessAddressCell/BusinessAddressCell.tsx
+++ web/src/components/BusinessAddressCell/BusinessAddressCell.tsx
@@ -4,13 +4,13 @@ import { Button } from '../ui/button'
 
 const BusinessAddressCell = () => {
   return (
-    <div className="ml-7 flex flex-wrap items-center justify-between text-muted/70 md:w-11/12">
-      <div className="flex items-center gap-1 xs:w-2/3">
+    <div className="ml-7 flex w-11/12 flex-wrap items-center justify-between text-muted/70">
+      <div className="flex w-3/4 items-center gap-1 xs:w-2/3">
         <MapPin className="size-4" />
         {'Oude Gracht 42, 3511AB Utrecht'}
       </div>
       <Button className="text-gray-500 hover:bg-black hover:text-destructive">
-        <X className="size-4" />
+        <X className="relative right-3 size-4" />
       </Button>
     </div>
   )
