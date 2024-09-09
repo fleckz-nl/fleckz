8f54fd79fa653f5109e07f2e30f6eed0bf00ae06
jazellemaira <jazellemaira@pm.me>
2024-09-09 21:48:42 +0200

    Refactor business address to BusinessAddressCell

--- web/src/components/BusinessAddressCell/BusinessAddressCell.tsx
+++ web/src/components/BusinessAddressCell/BusinessAddressCell.tsx
@@ -1,12 +1,17 @@
+import { MapPin, X } from 'lucide-react'
+
+import { Button } from '../ui/button'
+
 const BusinessAddressCell = () => {
   return (
-    <div>
-      <h2>{'BusinessAddressCell'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/BusinessAddressCell/BusinessAddressCell.tsx'
-        }
-      </p>
+    <div className="ml-7 flex flex-wrap items-center justify-between text-muted/70 md:w-11/12">
+      <div className="flex items-center gap-1 xs:w-2/3">
+        <MapPin className="size-4" />
+        {'Oude Gracht 42, 3511AB Utrecht'}
+      </div>
+      <Button className="text-gray-500 hover:bg-black hover:text-destructive">
+        <X className="size-4" />
+      </Button>
     </div>
   )
 }
