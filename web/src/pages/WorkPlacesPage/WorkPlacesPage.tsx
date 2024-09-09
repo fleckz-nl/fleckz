7385f35f4dd9e469b7fedb8bb3615c387e4edeff
jazellemaira <jazellemaira@pm.me>
2024-09-09 12:00:12 +0200

    Delete boilerplate content

--- web/src/pages/WorkPlacesPage/WorkPlacesPage.tsx
+++ web/src/pages/WorkPlacesPage/WorkPlacesPage.tsx
@@ -5,16 +5,6 @@ const WorkPlacesPage = () => {
   return (
     <>
       <Metadata title="WorkPlaces" description="WorkPlaces page" />
-
-      <h1>WorkPlacesPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/WorkPlacesPage/WorkPlacesPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>workPlaces</code>, link to me with `
-        <Link to={routes.workPlaces()}>WorkPlaces</Link>`
-      </p>
     </>
   )
 }
