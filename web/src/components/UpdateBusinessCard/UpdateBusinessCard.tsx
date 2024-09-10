0aa1405193d8831109c93e64269461e4a3f6fc39
Jazelle Carillo <jazellemaira@pm.me>
2024-09-10 19:36:39 +0200

    Add state for the businessName input and style the icons for editing and saving. Co-authored-by: Nami Sunami <nsunami@pm.me>

--- web/src/components/UpdateBusinessCard/UpdateBusinessCard.tsx
+++ web/src/components/UpdateBusinessCard/UpdateBusinessCard.tsx
@@ -1,19 +1,45 @@
-import { Building2, Edit, MapPin, Plus, X } from 'lucide-react'
+import { useRef, useState } from 'react'
+
+import { Building2, Check, Edit, MapPin, Plus, X } from 'lucide-react'
 
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import { Separator } from '../ui/separator'
 
 const UpdateBusinessCard = () => {
+  const [showInput, setShowInput] = useState(false)
+  const businessNameInputRef = useRef<HTMLInputElement>(null)
+  const [businessName, setBusinessName] = useState('BedrijfNaam')
   return (
     <>
-      <div className="flex w-full items-center gap-1 xs:gap-2">
+      <div className="flex w-full items-center gap-2">
         <Building2 />
-        <Input
-          defaultValue={'BedrijfNaam'}
-          className="text-md border-dashed border-gray-600/60"
-        ></Input>
-        <Edit className="relative right-8 text-gray-600" />
+        {showInput ? (
+          <Input
+            ref={businessNameInputRef}
+            className="text-md"
+            onBlur={() => setShowInput(false)}
+            value={businessName}
+            onChange={(e) => setBusinessName(e.target.value)}
+          />
+        ) : (
+          <span>{businessName}</span>
+        )}
+        {showInput ? (
+          <Check
+            className="rounded-full bg-gray-600 p-1 hover:bg-lime-400 hover:text-black focus:bg-lime-400"
+            onClick={() => setShowInput(false)}
+          />
+        ) : (
+          <Edit
+            onClick={() => {
+              setShowInput(true)
+              businessNameInputRef.current.select()
+              businessNameInputRef.current.focus()
+            }}
+            className=" size-5 text-gray-600 hover:text-accent"
+          />
+        )}
       </div>
       <div className="flex flex-col gap-3">
         <div className="ml-7 flex w-11/12 flex-wrap items-center justify-between text-muted/70">
@@ -26,7 +52,7 @@ const UpdateBusinessCard = () => {
           </Button>
         </div>
         <div className="container mt-2 flex flex-wrap items-center justify-end gap-4 xs:gap-2 md:justify-start">
-          <Input className="md:w-1/2"></Input>
+          <Input className="md:w-1/2" />
           <Button className="center px-2 text-accent hover:bg-gray-600 hover:text-black">
             <Plus className="mr-1 size-4" />
             Adres toevoegen
