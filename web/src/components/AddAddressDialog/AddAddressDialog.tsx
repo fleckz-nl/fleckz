d1b83f77a2e9abded12e57e756016e329af76a4b
Jazelle Carillo <jazellemaira@pm.me>
2024-09-10 14:04:49 +0200

    Delete boilerplate content

--- web/src/components/AddAddressDialog/AddAddressDialog.tsx
+++ web/src/components/AddAddressDialog/AddAddressDialog.tsx
@@ -1,14 +1,5 @@
 const AddAddressDialog = () => {
-  return (
-    <div>
-      <h2>{'AddAddressDialog'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/AddAddressDialog/AddAddressDialog.tsx'
-        }
-      </p>
-    </div>
-  )
+  return <></>
 }
 
 export default AddAddressDialog
