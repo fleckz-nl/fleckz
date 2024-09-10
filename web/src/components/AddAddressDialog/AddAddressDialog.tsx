08b03722c8312eb68a271502f6831800c77152bc
Jazelle Carillo <jazellemaira@pm.me>
2024-09-10 16:29:42 +0200

    Create a dialog button

--- web/src/components/AddAddressDialog/AddAddressDialog.tsx
+++ web/src/components/AddAddressDialog/AddAddressDialog.tsx
@@ -1,6 +1,15 @@
+import { Button } from '../ui/button'
+import {
   Dialog,
+  DialogTrigger,
+} from '../ui/dialog'
 const AddAddressDialog = () => {
     <Dialog>
+      <DialogTrigger asChild>
+        <Button className="mx-auto bg-white/20 p-6 text-lg text-white hover:bg-white/20 hover:text-accent">
+          Bedrijf toevoegen
+        </Button>
+      </DialogTrigger>
     </Dialog>
 }
 
