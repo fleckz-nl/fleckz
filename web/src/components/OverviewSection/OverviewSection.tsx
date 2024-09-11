f3d5acb92bbcb2b057577193226c298883994da4
Jazelle Maira Carillo <jazellemaira@pm.me>
2024-09-11 08:36:46 +0000

    Convert header to a flex and center elements vertically

--- web/src/components/OverviewSection/OverviewSection.tsx
+++ web/src/components/OverviewSection/OverviewSection.tsx
@@ -8,7 +8,10 @@ export function OverviewSection({ children, ...props }) {
 
 export function OverviewHeader({ children, ...props }) {
   return (
-    <h2 className="mb-2 text-xl font-bold text-white/90" {...props}>
+    <h2
+      className="mb-2 flex items-center text-xl font-bold text-white/90"
+      {...props}
+    >
       {children}
     </h2>
   )
