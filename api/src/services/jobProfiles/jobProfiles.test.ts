ed83eed3e20721a89ba473861db601922bccf776
Nami Sunami <nsunami@pm.me>
2024-09-11 15:56:49 +0000

    Delete qualityNeeded from jobProfile services

--- api/src/services/jobProfiles/jobProfiles.test.ts
+++ api/src/services/jobProfiles/jobProfiles.test.ts
@@ -34,33 +34,40 @@ describe('jobProfiles', () => {
   scenario('creates a jobProfile', async () => {
     const result = await createJobProfile({
       input: {
-        updatedAt: '2024-08-07T13:45:31.658Z',
-        name: 'String',
-        qualityNeeded: 4167938,
-        yearsOfExp: 1403786,
-        hourlyWageMin: 4618173.700980528,
-        hourlyWageMax: 7533275.579213512,
+        name: 'Salesmanager',
+        hourlyWageMin: 30,
+        hourlyWageMax: 55,
+        yearsOfExp: 4,
+        qualityNeeded: 4,
+        maxTravelDistance: 50,
+        kmAllowance: 0.25,
+        isCarAvailable: true,
+        isTravelReimbursed: true,
+        totalBudgetPerHour: 55,
+        comment:
+          'Sterke onderhandelingsvaardigheden en ervaring in B2B-verkoop is een must.',
       },
     })
 
     expect(result.updatedAt).toEqual(new Date('2024-08-07T13:45:31.658Z'))
     expect(result.name).toEqual('String')
-    expect(result.qualityNeeded).toEqual(4167938)
-    expect(result.yearsOfExp).toEqual(1403786)
-    expect(result.hourlyWageMin).toEqual(4618173.700980528)
-    expect(result.hourlyWageMax).toEqual(7533275.579213512)
+    expect(result.qualityNeeded).toEqual(4)
+    expect(result.yearsOfExp).toEqual(4)
+    expect(result.hourlyWageMin).toEqual(30)
+    expect(result.hourlyWageMax).toEqual(55)
   })
 
   scenario('updates a jobProfile', async (scenario: StandardScenario) => {
+    const newJobProfileName = 'New job profile name'
     const original = (await jobProfile({
       id: scenario.jobProfile.one.id,
     })) as JobProfile
     const result = await updateJobProfile({
       id: original.id,
-      input: { updatedAt: '2024-08-08T13:45:31.658Z' },
+      input: { name: newJobProfileName },
     })
 
-    expect(result.updatedAt).toEqual(new Date('2024-08-08T13:45:31.658Z'))
+    expect(result.updatedAt).toEqual(newJobProfileName)
   })
 
   scenario('deletes a jobProfile', async (scenario: StandardScenario) => {
