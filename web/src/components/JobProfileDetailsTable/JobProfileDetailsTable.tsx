edf690b4eaa88319c86bb1be9cd342193afa89c2
Jazelle Carillo <jazellemaira@pm.me>
2024-09-12 14:27:34 +0200

    Remove left margin

--- web/src/components/JobProfileDetailsTable/JobProfileDetailsTable.tsx
+++ web/src/components/JobProfileDetailsTable/JobProfileDetailsTable.tsx
@@ -10,7 +10,7 @@ const JobProfileDetailsTable = ({
   jobProfile,
 }: JobProfileDetailsTableProps) => {
   return (
-    <section id="profile-table" className="ml-4 w-full max-w-md">
+    <section id="profile-table" className="w-full max-w-md">
       <table>
         <thead>
           <tr>
