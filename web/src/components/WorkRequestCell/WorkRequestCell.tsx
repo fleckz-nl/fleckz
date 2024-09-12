69059ee8f73757ed300df2f85390efee22629c39
Jazelle Carillo <jazellemaira@pm.me>
2024-09-12 15:37:36 +0200

    Make the container into a grid on small screens

--- web/src/components/WorkRequestCell/WorkRequestCell.tsx
+++ web/src/components/WorkRequestCell/WorkRequestCell.tsx
@@ -100,14 +100,14 @@ export const Success = ({
 }: CellSuccessProps<FindWorkRequestQuery, FindWorkRequestQueryVariables> &
   SuccessProps) => {
   return (
-    <div className="flex min-h-screen w-full flex-wrap justify-between">
+    <div className="grid min-h-screen w-full justify-between xs:flex xs:flex-wrap">
       <div className="flex flex-grow flex-wrap justify-center bg-white">
         <div className="w-full bg-white pl-2 pt-4 text-xl font-bold hover:text-accent xs:mb-4 xs:pl-8">
           <Link to={routes.overview()} className="flex items-center">
             <ArrowLeft className="mr-1 inline" /> Overzicht
           </Link>
         </div>
-        <div className="container flex max-w-7xl flex-col items-center gap-8 pt-4 xs:pt-0">
+        <div className="container flex max-w-7xl flex-col items-center gap-8 pt-4 xs:pt-0 ">
           <div className="flex w-full flex-wrap items-center justify-center gap-4 xs:justify-between">
             <section className="px-0 xs:mx-auto">
               <RequestStatusCard
