5e175105753ff319cce553fac87b1d009950d3da
Jazelle Carillo <jazellemaira@pm.me>
2024-09-14 16:58:16 +0200

    Create content container

--- web/src/pages/TempAgenciesPage/TempAgenciesPage.tsx
+++ web/src/pages/TempAgenciesPage/TempAgenciesPage.tsx
@@ -4,6 +4,9 @@ const TempAgenciesPage = () => {
   return (
     <>
       <Metadata title="TempAgencies" description="TempAgencies page" />
+      <div className="mx-auto flex max-w-4xl flex-col">
+        <h1 className="text-xl font-bold text-white/90">Uitzendbureau</h1>
+      </div>
     </>
   )
 }
