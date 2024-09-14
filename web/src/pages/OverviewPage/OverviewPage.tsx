951866fd84a8a2e24e6cf927cbf8fdc7c249dcb0
Jazelle Carillo <jazellemaira@pm.me>
2024-09-14 16:59:59 +0200

    Reduce max width to be consistent with other pages

--- web/src/pages/OverviewPage/OverviewPage.tsx
+++ web/src/pages/OverviewPage/OverviewPage.tsx
@@ -6,7 +6,7 @@ const OverviewPage = () => {
   return (
     <>
       <Metadata title="Overview" description="Overview page" />
-      <section className="mx-auto max-w-6xl space-y-6">
+      <section className="mx-auto max-w-4xl space-y-6">
         <WorkRequestsCell />
       </section>
     </>
