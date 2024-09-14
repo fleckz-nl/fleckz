ff7987df28d5441e49c901562abd5dff6c2f66dc
Jazelle Carillo <jazellemaira@pm.me>
2024-09-14 17:22:00 +0200

    Add TempAgencyCard component

--- web/src/pages/TempAgenciesPage/TempAgenciesPage.tsx
+++ web/src/pages/TempAgenciesPage/TempAgenciesPage.tsx
@@ -1,11 +1,16 @@
 import { Metadata } from '@redwoodjs/web'
 
+import TempAgencyCard from 'src/components/TempAgencyCard/TempAgencyCard'
+
 const TempAgenciesPage = () => {
   return (
     <>
       <Metadata title="TempAgencies" description="TempAgencies page" />
       <div className="mx-auto flex max-w-4xl flex-col">
         <h1 className="text-xl font-bold text-white/90">Uitzendbureau</h1>
+        <div className="w-full pt-4 ">
+          <TempAgencyCard />
+        </div>
       </div>
     </>
   )
