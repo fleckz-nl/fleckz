3a899912cf3112252c39972654604b0033130beb
Jazelle Carillo <jazellemaira@pm.me>
2024-09-15 14:49:56 +0200

    Generate AddTempAgency dialog

---
+++ web/src/components/AddTempAgencyDialog/AddTempAgencyDialog.tsx
@@ -0,0 +1,14 @@
+const AddTempAgencyDialog = () => {
+  return (
+    <div>
+      <h2>{'AddTempAgencyDialog'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/AddTempAgencyDialog/AddTempAgencyDialog.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default AddTempAgencyDialog
