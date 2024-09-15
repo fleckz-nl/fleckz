86d6232250e9cfe26ae367dcdd5f6eb338640a64
Jazelle Carillo <jazellemaira@pm.me>
2024-09-15 14:55:47 +0200

    Replace boilerplate content with a dialog

--- web/src/components/AddTempAgencyDialog/AddTempAgencyDialog.tsx
+++ web/src/components/AddTempAgencyDialog/AddTempAgencyDialog.tsx
@@ -1,14 +1,7 @@
+import { Dialog } from '../ui/dialog'
+
 const AddTempAgencyDialog = () => {
-  return (
-    <div>
-      <h2>{'AddTempAgencyDialog'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/AddTempAgencyDialog/AddTempAgencyDialog.tsx'
-        }
-      </p>
-    </div>
-  )
+  return <Dialog></Dialog>
 }
 
 export default AddTempAgencyDialog
