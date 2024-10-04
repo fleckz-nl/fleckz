038c69adbe9d59f27fa20b8b44917a340ea466eb
Jazelle Carillo <jazellemaira@pm.me>
2024-10-04 16:36:47 +0200

    Generate AcceptedRequestsTable component

---
+++ web/src/components/AcceptedRequestsTable/AcceptedRequestsTable.tsx
@@ -0,0 +1,14 @@
+const AcceptedRequestsTable = () => {
+  return (
+    <div>
+      <h2>{"AcceptedRequestsTable"}</h2>
+      <p>
+        {
+          "Find me in ./web/src/components/AcceptedRequestsTable/AcceptedRequestsTable.tsx"
+        }
+      </p>
+    </div>
+  );
+};
+
+export default AcceptedRequestsTable;
