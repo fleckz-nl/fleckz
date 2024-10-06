d69f63efea576e5c080c03a83b5a0fd628b6b1c7
Jazelle Carillo <jazellemaira@pm.me>
2024-10-06 15:01:04 +0200

    Generate SortButton component

---
+++ web/src/components/SortButton/SortButton.tsx
@@ -0,0 +1,10 @@
+const SortButton = () => {
+  return (
+    <div>
+      <h2>{'SortButton'}</h2>
+      <p>{'Find me in ./web/src/components/SortButton/SortButton.tsx'}</p>
+    </div>
+  )
+}
+
+export default SortButton
