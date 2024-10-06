ec0ff5fb04317ddda1d3519c0a850720327c19fc
Jazelle Carillo <jazellemaira@pm.me>
2024-10-06 20:31:00 +0200

    Add select button elements

--- web/src/components/SortButton/SortButton.tsx
+++ web/src/components/SortButton/SortButton.tsx
@@ -1,9 +1,35 @@
+import React from 'react'
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select'
+
 const SortButton = () => {
   return (
-    <div>
-      <h2>{'SortButton'}</h2>
-      <p>{'Find me in ./web/src/components/SortButton/SortButton.tsx'}</p>
-    </div>
+    <Select>
+      <SelectTrigger className="w-[210px]">
+        <SelectValue placeholder="Sorteren" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Sorteren op</SelectLabel>
+          <SelectSeparator />
+          <SelectItem value="latest">Laatste</SelectItem>
+          <SelectItem value="oldest">Oudste</SelectItem>
+          <SelectItem value="recentlyUpdated">Onlangs bijgewerkt</SelectItem>
+          <SelectItem value="leastRecentlyUpdated">
+            Minst onlangs bijgewerkt
+          </SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   )
 }
 
