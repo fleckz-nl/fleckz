665e0262d9191a72348639bc95265671bc9dffdf
Jazelle Carillo <jazellemaira@pm.me>
2024-10-07 21:41:39 +0200

    feat(layout): Add tab elements

--- web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
+++ web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react'
+
+import { Badge } from 'web/src/components/ui/badge'
 import { Button } from 'web/src/components/ui/button'
 import {
   Drawer,
   DrawerContent,
   DrawerTrigger,
 } from 'web/src/components/ui/drawer'
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from 'web/src/components/ui/tabs'
+
 const ShiftConfirmationDrawer = () => {
   return (
     <Drawer>
@@ -12,6 +21,32 @@ const ShiftConfirmationDrawer = () => {
         <Button>Inchecken: Nu</Button>
       </DrawerTrigger>
       <DrawerContent className="container px-4">
+        <Tabs defaultValue="shiftCheckIn">
+          <TabsList className="mx-auto mt-2 grid grid-cols-3 gap-2">
+            <TabsTrigger value="shiftCheckIn">Inchecken</TabsTrigger>
+            <TabsTrigger value="shiftCheckOut">Uitchecken</TabsTrigger>
+            <TabsTrigger value="shiftSummary">Samenvatting</TabsTrigger>
+          </TabsList>
+          <TabsContent
+            value="shiftCheckIn"
+            className="flex w-full flex-col pb-4"
+          >
+            <h3 className="flex items-center justify-between">
+              <div className="flex flex-col">
+                <span className="font-semibold">Achternaam Voornaam</span>
+                <Badge className="w-fit">Uitzendbureau</Badge>
+              </div>
+              <Button>Nu</Button>
+            </h3>
+            <div className="mx-auto flex items-center justify-center gap-5 py-10">
+              <span className="mx-auto text-2xl">DD MMM YYYY</span>
+              <input
+                type="time"
+                className="rounded-sm bg-black px-2 text-center text-2xl text-white"
+              />
+            </div>
+          </TabsContent>
+        </Tabs>
       </DrawerContent>
     </Drawer>
   )
