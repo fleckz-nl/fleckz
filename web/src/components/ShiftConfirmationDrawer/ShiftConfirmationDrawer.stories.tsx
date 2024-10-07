f8137a0d97efba97434d347cec5c88a56b719129
Jazelle Carillo <jazellemaira@pm.me>
2024-10-07 15:21:57 +0200

    feat: Generate ShiftConfirmationDrawer component

---
+++ web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.stories.tsx
@@ -0,0 +1,26 @@
+// Pass props to your component by passing an `args` object to your story
+//
+// ```tsx
+// export const Primary: Story = {
+//  args: {
+//    propName: propValue
+//  }
+// }
+// ```
+//
+// See https://storybook.js.org/docs/react/writing-stories/args.
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ShiftConfirmationDrawer from './ShiftConfirmationDrawer'
+
+const meta: Meta<typeof ShiftConfirmationDrawer> = {
+  component: ShiftConfirmationDrawer,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof ShiftConfirmationDrawer>
+
+export const Primary: Story = {}
---
+++ web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+const ShiftConfirmationDrawer = () => {
+  return (
+    <div>
+      <h2>{'ShiftConfirmationDrawer'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default ShiftConfirmationDrawer
