ad65ccd4f71cbd621a42955ae6d98d863b64ff64
Jazelle Carillo <jazellemaira@pm.me>
2024-10-07 10:31:22 +0200

    feat: add SearchInput component

---
+++ web/src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const SearchInput = () => {
+  return (
+    <div>
+      <h2>{'SearchInput'}</h2>
+      <p>{'Find me in ./web/src/components/SearchInput/SearchInput.tsx'}</p>
+    </div>
+  )
+}
+
+export default SearchInput
