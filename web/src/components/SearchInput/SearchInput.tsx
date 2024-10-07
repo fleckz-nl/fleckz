b47698ea9db7a1259f2eac537eb446d6ffd5e9f6
Jazelle Carillo <jazellemaira@pm.me>
2024-10-07 12:26:09 +0200

    fix(ui): adjust color scheme to match brand guidelines

--- web/src/components/SearchInput/SearchInput.tsx
+++ web/src/components/SearchInput/SearchInput.tsx
@@ -7,9 +7,13 @@ import { Input } from 'web/src/components/ui/input'
 const SearchInput = () => {
   return (
     <Form>
-      <div className="relative flex items-center text-primary-foreground">
+      <div className="relative flex items-center text-white/70">
         <Search className="absolute mx-2 size-4" />
-        <Input type="text" placeholder="Zoek maar door" className="pl-8" />
+        <Input
+          type="text"
+          placeholder="Zoek maar door"
+          className="border-primary-foreground/20 bg-gray-800/50 pl-8 placeholder:text-white/70 focus:bg-black focus:text-white"
+        />
       </div>
     </Form>
   )
