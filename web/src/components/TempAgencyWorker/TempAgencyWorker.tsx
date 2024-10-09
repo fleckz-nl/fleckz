b3b78ffe96449f3cc0f1d92ecbe144e5049a5727
Jazelle Carillo <jazellemaira@pm.me>
2024-10-09 11:05:54 +0200

    feat: Generate TempAgencyWorker component

---
+++ web/src/components/TempAgencyWorker/TempAgencyWorker.tsx
@@ -0,0 +1,14 @@
+const TempAgencyWorker = () => {
+  return (
+    <div>
+      <h2>{'TempAgencyWorker'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/TempAgencyWorker/TempAgencyWorker.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default TempAgencyWorker
