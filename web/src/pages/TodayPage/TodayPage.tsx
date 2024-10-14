b6f5235ede9451ba11e3e48cbc811b71c86758c9
Jazelle Carillo <jazellemaira@pm.me>
2024-10-14 17:16:48 +0200

    feat(layout): Add section header and subheader

--- web/src/pages/TodayPage/TodayPage.tsx
+++ web/src/pages/TodayPage/TodayPage.tsx
@@ -1,17 +1,39 @@
 // import { Link, routes } from '@redwoodjs/router'
+import { Users } from 'lucide-react'
+import { Separator } from 'web/src/components/ui/separator'
+
 import { Metadata } from '@redwoodjs/web'
 
 const TodayPage = () => {
   return (
     <>
       <Metadata title="Today" description="Today page" />
-      <div className="mx-auto flex max-w-4xl flex-col ">
+      <div className="mx-auto flex max-w-4xl flex-col">
         <div className="flex flex-col items-center text-xl font-bold">
           <h1 className="text-white/90">Vandaag</h1>
           <span className="text-2xl font-medium text-accent">
             14 Oktober 2024
           </span>
         </div>
+        <section className="flex flex-col items-center">
+          <Separator className="mt-4 bg-primary-foreground/20" />
+          <h3 className="center mt-2 gap-2 text-center">
+            <div className="flex gap-1">
+              <span>08:00</span>
+              <span>-</span>
+              <span>15:00</span>
+            </div>
+            <Separator
+              orientation="vertical"
+              className="h-4 w-[2px] text-accent"
+            />
+            <span>Funtienaam</span>
+          </h3>
+          <div className="center gap-1">
+            <span>6</span>
+            <Users className="size-5" />
+          </div>
+        </section>
       </div>
     </>
   )
