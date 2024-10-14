ef94ff4c136b9bc9261ae76a8a0d0d10c47fe148
Nami Sunami <nsunami@pm.me>
2024-10-14 18:57:43 +0200

    feat(CheckInTab): Use a component for check-in tab and pass shift information

--- web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
+++ web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
@@ -14,6 +14,8 @@ import {
   TabsTrigger,
 } from 'web/src/components/ui/tabs'
 
+import CheckInTab from 'src/components/CheckInTab/CheckInTab'
+
 import TempAgencyWorker from 'src/components/TempAgencyWorker'
 import TimePicker from 'src/components/TimePicker'
 import { Badge } from 'src/components/ui/badge'
@@ -22,7 +24,7 @@ type ShiftConfirmationDrawerProps = {
   time: string
 }
 
-const ShiftConfirmationDrawer = ({ time }: ShiftConfirmationDrawerProps) => {
+const ShiftConfirmationDrawer = ({ shift }: ShiftConfirmationDrawerProps) => {
   return (
     <Drawer>
       <DrawerTrigger asChild>
@@ -53,26 +55,7 @@ const ShiftConfirmationDrawer = ({ time }: ShiftConfirmationDrawerProps) => {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="shiftCheckIn">
-            <div className="my-4 flex h-[250px] flex-col justify-between">
-              <div className="flex flex-col">
-                <h3 className="flex flex-wrap items-center justify-between">
-                  <div className="flex gap-4">
-                    <TempAgencyWorker />
-                    <Badge
-                      variant="outline"
-                      className=" h-4 bg-lime-500 text-black"
-                    >
-                      In
-                    </Badge>
-                  </div>
-                  <Button className="bg-gray-900 text-accent">Nu</Button>
-                </h3>
-                <TimePicker time={time} />
-              </div>
-              <Button className="bg-accent/80 text-black hover:bg-accent hover:text-black sm:mx-auto">
-                Bevestigen
-              </Button>
-            </div>
+            <CheckInTab shift={shift} />
           </TabsContent>
           <TabsContent value="shiftCheckOut">
             <div className="my-4 flex h-[250px] flex-col justify-between">
