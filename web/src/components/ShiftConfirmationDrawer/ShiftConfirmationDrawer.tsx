65c8b7890c4ec8d50f4fda555f9cf25b1bd1bace
Nami Sunami <nsunami@pm.me>
2024-10-14 20:58:40 +0200

    feat(ShiftConfirmation): Implement CheckOutTab

--- web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
+++ web/src/components/ShiftConfirmationDrawer/ShiftConfirmationDrawer.tsx
@@ -1,6 +1,4 @@
-import * as React from 'react'
-
-import { ThumbsDown, ThumbsUp } from 'lucide-react'
+import { FindWorkRequestQuery } from 'types/graphql'
 import { Button } from 'web/src/components/ui/button'
 import {
   Drawer,
@@ -15,10 +13,8 @@ import {
 } from 'web/src/components/ui/tabs'
 
 import CheckInTab from 'src/components/CheckInTab/CheckInTab'
-
+import CheckOutTab from 'src/components/CheckOutTab/CheckOutTab'
 import TempAgencyWorker from 'src/components/TempAgencyWorker'
-import TimePicker from 'src/components/TimePicker'
-import { Badge } from 'src/components/ui/badge'
 
 type ShiftConfirmationDrawerProps = {
   time: string
@@ -58,34 +54,7 @@ const ShiftConfirmationDrawer = ({ shift }: ShiftConfirmationDrawerProps) => {
             <CheckInTab shift={shift} />
           </TabsContent>
           <TabsContent value="shiftCheckOut">
-            <div className="my-4 flex h-[250px] flex-col justify-between">
-              <div className="flex flex-col">
-                <h3 className="flex flex-wrap items-center justify-between">
-                  <div className="flex gap-4">
-                    <TempAgencyWorker />
-                    <Badge
-                      variant="outline"
-                      className=" h-4 bg-red-500 text-black"
-                    >
-                      Uit
-                    </Badge>
-                  </div>
-                  <Button className="bg-gray-900 text-accent">Nu</Button>
-                </h3>
-                <TimePicker time={time} />
-                <div className="flex gap-2 self-end">
-                  <Button className="bg-gray-900 p-2 text-accent">
-                    <ThumbsUp className="size-5" />
-                  </Button>
-                  <Button className="bg-gray-900 p-2 text-accent">
-                    <ThumbsDown className="size-5" />
-                  </Button>
-                </div>
-              </div>
-              <Button className="bg-accent/80 text-black hover:bg-accent hover:text-black sm:mx-auto">
-                Bevestigen
-              </Button>
-            </div>
+            <CheckOutTab shift={shift} />
           </TabsContent>
           <TabsContent value="shiftSummary">
             <div className="my-4 flex h-[250px] flex-col justify-between">
