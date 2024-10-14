5d6d85277db9c9cecfb93d65ade08d1e01f9d934
Nami Sunami <nsunami@pm.me>
2024-10-14 21:03:54 +0200

    chore(CheckOutTab): Use the mocked shift from the mocked data from Cell

--- web/src/components/CheckOutTab/CheckOutTab.test.tsx
+++ web/src/components/CheckOutTab/CheckOutTab.test.tsx
@@ -1,6 +1,6 @@
-import { FindWorkRequestQuery } from 'types/graphql'
-
 import { render } from '@redwoodjs/testing/web'
+
+import { mockShifts } from 'src/components/WorkRequestCell/WorkRequestCell.mock'
 
 import CheckOutTab from './CheckOutTab'
 
@@ -10,33 +10,7 @@ import CheckOutTab from './CheckOutTab'
 describe('CheckOutTab', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<CheckOutTab shift={mockShift} />)
+      render(<CheckOutTab shift={mockShifts[0]} />)
     }).not.toThrow()
   })
 })
-
-const mockShift: FindWorkRequestQuery['workRequest']['shifts'][0] = {
-  __typename: 'Shift',
-  id: 'mock_shift_4_1',
-  name: 'Ploegendienst 1',
-  status: 'FULFILLED',
-  rating: null,
-  workerName: null,
-  checkedInAt: null,
-  checkedOutAt: null,
-  tempAgency: {
-    __typename: 'TempAgency',
-    id: 'agency_2',
-    name: 'SnelAanHetWerk',
-    email: 'info@snelaanhetwerk.nl',
-    phone: '+31 12 345 6789',
-    address: {
-      __typename: 'Address',
-      street: 'Oude Gracht',
-      houseNumber: '42',
-      houseNumberAddition: 'B',
-      postalCode: '3511AB',
-      city: 'Utrecht',
-    },
-  },
-}
