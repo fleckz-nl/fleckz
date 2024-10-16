68d57cea2752d5bcce401aa6e477fc51241f07d0
Nami Sunami <nsunami@pm.me>
2024-10-16 22:03:26 +0200

    feat(CheckOutTab): Update tests

--- web/src/components/CheckOutTab/CheckOutTab.test.tsx
+++ web/src/components/CheckOutTab/CheckOutTab.test.tsx
@@ -1,6 +1,4 @@
-import { render } from '@redwoodjs/testing/web'
-
-import { mockShifts } from 'src/components/WorkRequestCell/WorkRequestCell.mock'
+import { render, fireEvent } from '@redwoodjs/testing/web'
 
 import CheckOutTab from './CheckOutTab'
 
@@ -9,8 +7,58 @@ import CheckOutTab from './CheckOutTab'
 
 describe('CheckOutTab', () => {
   it('renders successfully', () => {
-    expect(() => {
-      render(<CheckOutTab shift={mockShifts[0]} />)
-    }).not.toThrow()
+    const { getByText } = render(
+      <CheckOutTab
+        checkOutAt={new Date()}
+        setCheckOutAt={() => {}}
+        loading={false}
+        handleCheckOut={() => {}}
+        shiftRating={0}
+        setShiftRating={() => {}}
+      />
+    )
+
+    expect(getByText('Uit')).toBeInTheDocument()
+    expect(getByText('Nu')).toBeInTheDocument()
+  })
+
+  it('calls handleClickNow when button is clicked', () => {
+    const setCheckOutAt = jest.fn()
+    const { getByText } = render(
+      <CheckOutTab
+        checkOutAt={new Date()}
+        setCheckOutAt={setCheckOutAt}
+        loading={false}
+        handleCheckOut={() => {}}
+        shiftRating={0}
+        setShiftRating={() => {}}
+      />
+    )
+
+    const button = getByText('Nu')
+    fireEvent.click(button)
+
+    expect(setCheckOutAt).toHaveBeenCalledTimes(1)
+    expect(setCheckOutAt).toHaveBeenCalledWith(new Date())
+  })
+
+  it('calls setCheckInAt when TimePicker date changes', () => {
+    const setCheckOutAt = jest.fn()
+    const { getByDisplayValue } = render(
+      <CheckOutTab
+        checkOutAt={new Date('2024-10-16T21:41:00')}
+        setCheckOutAt={setCheckOutAt}
+        loading={false}
+        handleCheckOut={() => {}}
+        shiftRating={0}
+        setShiftRating={() => {}}
+      />
+    )
+
+    const timeInput = getByDisplayValue('21:41')
+    fireEvent.change(timeInput, { target: { value: '22:30' } })
+
+    expect(setCheckOutAt).toHaveBeenCalledTimes(1)
+    expect(setCheckOutAt).toHaveBeenCalledWith(new Date('2024-10-16T22:30:00'))
   })
 })
