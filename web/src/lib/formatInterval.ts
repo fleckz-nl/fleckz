abcf1ff1e00bf7ec464da693680bee290200c526
Nami Sunami <nsunami@pm.me>
2024-10-16 23:20:12 +0200

    feat(formatInterval): Localize the interval values in Dutch

--- web/src/lib/formatInterval.ts
+++ web/src/lib/formatInterval.ts
@@ -1,14 +1,19 @@
 import { format, isSameDay, isSameMonth } from 'date-fns'
+import { nl } from 'date-fns/locale/nl'
 
 export function formatInterval(date1: Date, date2: Date) {
   const isMultiDay = !isSameDay(date1, date2)
   if (isMultiDay) {
     const isMultiMonth = !isSameMonth(date1, date2)
     if (isMultiMonth)
-      return `${format(date1, 'd MMMM')}–${format(date2, 'd MMMM')} ${format(date2, 'yyyy')}`
+      return `${formatToNL(date1, 'd MMMM')}–${formatToNL(date2, 'd MMMM')} ${formatToNL(date2, 'yyyy')}`
 
-    return `${format(date1, 'd')}–${format(date2, 'd MMMM')} ${format(date2, 'yyyy')}`
+    return `${formatToNL(date1, 'd')}–${formatToNL(date2, 'd MMMM')} ${formatToNL(date2, 'yyyy')}`
   }
 
-  return format(date2, 'd MMMM yyyy')
+  return formatToNL(date2, 'd MMMM yyyy')
+}
+
+function formatToNL(date: Date, pattern: string) {
+  return format(date, pattern, { locale: nl })
 }
