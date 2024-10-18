b2073531901096c9256be1ac3b8adde4d7fddb70
Jazelle Carillo <jazellemaira@pm.me>
2024-10-18 14:03:03 +0200

    feat(layout): Create a template for invoice list item card

--- web/src/pages/InvoicePage/InvoicePage.tsx
+++ web/src/pages/InvoicePage/InvoicePage.tsx
@@ -1,12 +1,32 @@
 // import { Link, routes } from '@redwoodjs/router'
+import { Download, FileSearch, FileText } from 'lucide-react'
+import { Button } from 'web/src/components/ui/button'
+
 import { Metadata } from '@redwoodjs/web'
 
 const InvoicePage = () => {
   return (
     <>
       <Metadata title="Invoice" description="Invoice page" />
-      <div className="relative -left-4 -top-4 h-[200vh] w-screen bg-white">
-        <div className="mx-auto flex max-w-4xl flex-col"></div>
+      <div className="relative -left-4 -top-4 min-h-[120vh] w-screen bg-white">
+        <div className="mx-auto flex max-w-4xl flex-col">
+          <ul className="flex w-full flex-col items-center gap-1 pb-32 pt-20">
+            <li className="flex h-12 w-11/12 items-center justify-between rounded-md bg-primary/20 px-5 text-primary/70 xs:w-4/5">
+              <div className="flex w-3/4 gap-1">
+                <FileText />
+                <span className="font-semibold">12 Okt 2024</span>
+              </div>
+              <div className="flex gap-4">
+                <Button className="bg-primary p-0">
+                  <FileSearch />
+                </Button>
+                <Button className="bg-primary p-0">
+                  <Download />
+                </Button>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   )
