5746d9bac783dc0c4ab47f3ed5801d3749127876
Jazelle Carillo <jazellemaira@pm.me>
2024-10-31 16:11:10 +0100

    feat: Generate dialog component for assigning shift worker

---
+++ web/src/components/AssignShiftWorkerDialog/AssignShiftWorkerDialog.tsx
@@ -0,0 +1,14 @@
+const AssignShiftWorkerDialog = () => {
+  return (
+    <div>
+      <h2>{'AssignShiftWorkerDialog'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/AssignShiftWorkerDialog/AssignShiftWorkerDialog.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default AssignShiftWorkerDialog
