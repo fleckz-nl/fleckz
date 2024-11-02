ffd2e5c419ac63b316417cb6e01794e2b1cf8429
Nami Sunami <nsunami@pm.me>
2024-11-02 09:30:31 +0100

    feat(ShiftTable): Delete the time column Since the time is rendered at the top of the table.

--- web/src/components/ShiftTable/ShiftTable.tsx
+++ web/src/components/ShiftTable/ShiftTable.tsx
@@ -58,11 +58,6 @@ const ShiftTable = ({ request, tempAgencies }: ShiftTableProps) => {
         accessorKey: 'tempAgency',
       },
       {
-        accessorKey: 'shiftTime',
-        header: 'Tijd',
-        cell: '09:00-13:30',
-      },
-      {
         accessorKey: 'agency',
         header: 'Uitzendbureau',
         cell: ({ row }) => (
