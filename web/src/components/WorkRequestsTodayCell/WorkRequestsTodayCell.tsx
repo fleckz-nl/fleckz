67a397bda44344bdc5268012eda0643485af14b9
Jazelle Carillo <jazellemaira@pm.me>
2024-11-02 18:24:19 +0100

    feat(layout): Improve empty state appearance.

--- web/src/components/WorkRequestsTodayCell/WorkRequestsTodayCell.tsx
+++ web/src/components/WorkRequestsTodayCell/WorkRequestsTodayCell.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
+
 import type {
   WorkRequestsTodayQuery,
   WorkRequestsTodayQueryVariables,
 } from 'types/graphql'
+import workers from 'web/public/images/workers.png'
 
 import type {
   CellSuccessProps,
@@ -10,6 +13,7 @@ import type {
 } from '@redwoodjs/web'
 
 import IndividualWorkRequestSection from 'src/components/IndividualWorkRequestSection/IndividualWorkRequestSection'
+import PlanWorkComponent from 'src/components/PlanWorkComponent/PlanWorkComponent'
 
 export const QUERY: TypedDocumentNode<
   WorkRequestsTodayQuery,
@@ -44,8 +48,20 @@ export const QUERY: TypedDocumentNode<
 
 export const Loading = () => <div>Loading...</div>
 
-export const Empty = () => <div>Empty</div>
-
+export const Empty = () => {
+  const [openDialog, setOpenDialog] = useState(false)
+  return (
+    <div className="center w-full flex-col">
+      <div className="center relative -top-14 flex-col">
+        <img src={workers} alt="workers illustration" className="size-56" />
+        <span className="text-center text-lg italic">
+          Vandaag geen werk, geen diensten.
+        </span>
+      </div>
+      <PlanWorkComponent open={openDialog} setOpen={setOpenDialog} />
+    </div>
+  )
+}
 export const Failure = ({
   error,
 }: CellFailureProps<WorkRequestsTodayQueryVariables>) => (
