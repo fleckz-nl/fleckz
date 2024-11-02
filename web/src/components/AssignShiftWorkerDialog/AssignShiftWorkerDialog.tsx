ff39d6a27c249ca8e6597c04ba37b6ffc15a7607
Jazelle Carillo <jazellemaira@pm.me>
2024-11-02 11:40:32 +0100

    feat: Create DialogContent layout

--- web/src/components/AssignShiftWorkerDialog/AssignShiftWorkerDialog.tsx
+++ web/src/components/AssignShiftWorkerDialog/AssignShiftWorkerDialog.tsx
@@ -1,5 +1,20 @@
 import { Button } from 'web/src/components/ui/button'
-import { Dialog, DialogTrigger } from 'web/src/components/ui/dialog'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from 'web/src/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from 'web/src/components/ui/form'
+import { Input } from 'web/src/components/ui/input'
 
 const AssignShiftWorkerDialog = () => {
   return (
@@ -9,6 +24,32 @@ const AssignShiftWorkerDialog = () => {
           Vul een dienst in
         </Button>
       </DialogTrigger>
+      <DialogContent className="max-h-screen overflow-y-scroll">
+        <DialogHeader>
+          <DialogTitle className="font-bold text-primary/80">
+            Vul een dienst in
+          </DialogTitle>
+          <DialogDescription>Vul de naam van de werknemer</DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form>
+            <FormField
+              name="street"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-primary-foreground">
+                    Naam Werknemer
+                  </FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </DialogContent>
     </Dialog>
   )
 }
