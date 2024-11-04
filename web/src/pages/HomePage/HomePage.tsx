f19a1a83e7a3720ead6190e6b6de47c13d4b95cc
Jazelle Carillo <jazellemaira@pm.me>
2024-11-04 17:39:36 +0100

    feat: Replace site slogan

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -66,7 +66,7 @@ const HomePage = () => {
               className="mx-auto w-48 drop-shadow-sm"
             />
             <section className="m-0 mt-2 px-2 text-center text-xl italic text-white/70 drop-shadow-sm md:text-2xl">
-              <p>Vind je de beste kandidaat, overal.</p>
+              <p>De zoekmachine voor uitzendkrchten</p>
             </section>
           </div>
           <div className="form-wrapper m-0 flex w-11/12 max-w-sm flex-col rounded-md bg-black drop-shadow-sm">
