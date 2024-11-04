481659e6741bcbfe325b9941581f23c7412d4d5a
Nami Sunami <nsunami@pm.me>
2024-11-04 21:42:40 +0100

    Revert "test(AssignWorker): Fix the test while here"
    This reverts commit 1e9a7d694998994562035aa8a798e9bb788f5cca.

--- web/src/components/AssignShiftWorkerDialog/AssignShiftWorkerDialog.test.tsx
+++ web/src/components/AssignShiftWorkerDialog/AssignShiftWorkerDialog.test.tsx
@@ -1,7 +1,5 @@
 import { render } from '@redwoodjs/testing/web'
 
-import { mockShifts } from 'src/components/WorkRequestCell/WorkRequestCell.mock'
-
 import AssignShiftWorkerDialog from './AssignShiftWorkerDialog'
 
 //   Improve this test with help from the Redwood Testing Doc:
@@ -10,7 +8,7 @@ import AssignShiftWorkerDialog from './AssignShiftWorkerDialog'
 describe('AssignShiftWorkerDialog', () => {
   it('renders successfully', () => {
     expect(() => {
-      render(<AssignShiftWorkerDialog shift={mockShifts[0]} />)
+      render(<AssignShiftWorkerDialog />)
     }).not.toThrow()
   })
 })
