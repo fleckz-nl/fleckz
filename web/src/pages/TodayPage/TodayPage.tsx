f7814ceeb3ca08e3246cf74fcc1016ffc44a9bfe
Jazelle Carillo <jazellemaira@pm.me>
2024-11-04 21:04:36 +0100

    feat(call-to-action-btns): Add PlanWorkDialog and link to PlanPage at the bottom of the page

--- web/src/pages/TodayPage/TodayPage.tsx
+++ web/src/pages/TodayPage/TodayPage.tsx
@@ -1,12 +1,18 @@
 // import { Link, routes } from '@redwoodjs/router'
+import { useState } from 'react'
+
 import { format } from 'date-fns/format'
 import { nl } from 'date-fns/locale/nl'
+import { Calendar } from 'lucide-react'
 
+import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
+import PlanWorkComponent from 'src/components/PlanWorkComponent/PlanWorkComponent'
 import WorkRequestsTodayCell from 'src/components/WorkRequestsTodayCell'
 
 const TodayPage = () => {
+  const [openDialog, setOpenDialog] = useState(false)
   return (
     <>
       <Metadata title="Today" description="Today page" />
@@ -18,6 +24,12 @@ const TodayPage = () => {
           </span>
         </div>
         <WorkRequestsTodayCell />
+        <div className="center mx-auto gap-2">
+          <PlanWorkComponent open={openDialog} setOpen={setOpenDialog} />
+          <Link to={routes.plan()}>
+            <Calendar className="relative -top-1 size-[35px] rounded-md border border-accent bg-transparent/10 p-1 text-white/80 hover:bg-muted-foreground/10 hover:text-accent" />
+          </Link>
+        </div>
       </div>
     </>
   )
