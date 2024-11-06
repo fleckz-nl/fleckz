a676af2b76a0086dba7bb493601dd78a7ce76fe0
Jazelle Carillo <jazellemaira@pm.me>
2024-11-06 22:28:51 +0100

    feat(layout): Create email and password form layout

--- web/src/components/OnboardingEmailAndPassword/OnboardingEmailAndPassword.tsx
+++ web/src/components/OnboardingEmailAndPassword/OnboardingEmailAndPassword.tsx
@@ -1,12 +1,64 @@
+import {
+  FieldError,
+  Form,
+  Label,
+  PasswordField,
+  Submit,
+  TextField,
+} from '@redwoodjs/forms'
 const OnboardingEmailAndPassword = () => {
   return (
-    <div>
-      <h2>{'OnboardingEmailAndPassword'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/OnboardingEmailAndPassword/OnboardingEmailAndPassword.tsx'
-        }
-      </p>
+    <div className="rw-scaffold mx-auto my-40 max-w-md py-4">
+      <div className="rw-segment-main">
+        <div className="rw-form-wrapper">
+          <Form onSubmit={onSubmit} className="rw-form-wrapper">
+            <Label
+              name="email"
+              className="rw-label"
+              errorClassName="rw-label rw-label-error"
+            >
+              Email
+            </Label>
+            <TextField
+              name="email"
+              className="rw-input"
+              errorClassName="rw-input rw-input-error"
+              ref={emailRef}
+              validation={{
+                required: {
+                  value: true,
+                  message: 'Email is verplicht',
+                },
+              }}
+            />
+            <FieldError name="email" className="rw-field-error" />
+
+            <Label
+              name="password"
+              className="rw-label"
+              errorClassName="rw-label rw-label-error"
+            >
+              Wachtwoord
+            </Label>
+            <PasswordField
+              name="password"
+              className="rw-input text-accent"
+              errorClassName="rw-input rw-input-error"
+              autoComplete="current-password"
+              validation={{
+                required: {
+                  value: true,
+                  message: 'Wachtwoord is verplicht',
+                },
+              }}
+            />
+            <FieldError name="password" className="rw-field-error" />
+            <div className="rw-button-group">
+              <Submit className="rw-button rw-button-accent">Aanmelden</Submit>
+            </div>
+          </Form>
+        </div>
+      </div>
     </div>
   )
 }
