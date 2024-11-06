8f5b8cd277a5c32bc7fbc433710c8af230f3796f
Jazelle Carillo <jazellemaira@pm.me>
2024-11-06 15:40:20 +0100

    feat(welcome-component): Style onboarding welcome component

--- web/src/components/OnboardingWelcome/OnboardingWelcome.tsx
+++ web/src/components/OnboardingWelcome/OnboardingWelcome.tsx
@@ -1,12 +1,17 @@
+import logo from 'web/public/images/logo-white-text.png'
+
+import { Button } from 'src/components/ui/button'
+
 const OnboardingWelcome = () => {
   return (
-    <div>
-      <h2>{'OnboardingWelcome'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/OnboardingWelcome/OnboardingWelcome.tsx'
-        }
-      </p>
+    <div className="center mx-auto flex-col gap-4 py-40">
+      <img src={logo} alt="logo" className="w-56" />
+      <span className="text-center text-xl italic">
+        De zoekmachine voor uitzendkrachten
+      </span>
+      <Button className="my-20 bg-accent text-lg text-primary hover:bg-black hover:text-accent">
+        Begginen
+      </Button>
     </div>
   )
 }
