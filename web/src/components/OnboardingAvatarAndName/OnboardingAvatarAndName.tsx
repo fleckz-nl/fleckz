0f3a4ab2d52d85fd5910917c35ef999b4a53afa8
Jazelle Carillo <jazellemaira@pm.me>
2024-11-07 12:18:17 +0100

    feat(AvatarAndName): Create avatar and name card

--- web/src/components/OnboardingAvatarAndName/OnboardingAvatarAndName.tsx
+++ web/src/components/OnboardingAvatarAndName/OnboardingAvatarAndName.tsx
@@ -1,12 +1,15 @@
 const OnboardingAvatarAndName = () => {
   return (
-    <div>
-      <h2>{'OnboardingAvatarAndName'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/OnboardingAvatarAndName/OnboardingAvatarAndName.tsx'
-        }
-      </p>
+    <div className="rw-scaffold mx-auto my-40 max-w-md py-4">
+      <div className="rw-segment-main">
+        <div className="rw-form-wrapper">
+          <p className="break-words">
+            {
+              'Find me in ./web/src/components/OnboardingAvatarAndName/OnboardingAvatarAndName.tsx'
+            }
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
