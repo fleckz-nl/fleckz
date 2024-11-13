65c9ea2472f7125ac9e9e3ff8cb506a157550863
Jazelle Carillo <jazellemaira@pm.me>
2024-11-13 17:52:40 +0100

    feat(InvoiceTable): Generate invoice table component

---
+++ web/src/components/InvoiceTable/InvoiceTable.tsx
@@ -0,0 +1,10 @@
+const InvoiceTable = () => {
+  return (
+    <div>
+      <h2>{'InvoiceTable'}</h2>
+      <p>{'Find me in ./web/src/components/InvoiceTable/InvoiceTable.tsx'}</p>
+    </div>
+  )
+}
+
+export default InvoiceTable
