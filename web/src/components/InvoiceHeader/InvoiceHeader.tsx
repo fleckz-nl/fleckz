9cb83a15e0c41123e23736c73e859a6b449ae030
Jazelle Carillo <jazellemaira@pm.me>
2024-11-13 14:06:27 +0100

    feat(InvoiceHeader): Generate invoice header

---
+++ web/src/components/InvoiceHeader/InvoiceHeader.tsx
@@ -0,0 +1,10 @@
+const InvoiceHeader = () => {
+  return (
+    <div>
+      <h2>{'InvoiceHeader'}</h2>
+      <p>{'Find me in ./web/src/components/InvoiceHeader/InvoiceHeader.tsx'}</p>
+    </div>
+  )
+}
+
+export default InvoiceHeader
