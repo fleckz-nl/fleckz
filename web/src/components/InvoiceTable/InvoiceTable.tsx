714092339eb07066736d9f1358c10dc932cb6792
Jazelle Carillo <jazellemaira@pm.me>
2024-11-15 15:48:51 +0100

    feat(InvoiceTable): Style table elements

--- web/src/components/InvoiceTable/InvoiceTable.tsx
+++ web/src/components/InvoiceTable/InvoiceTable.tsx
@@ -12,29 +12,29 @@ import {
 const InvoiceTable = () => {
   return (
     <Table>
-      <TableHeader>
+      <TableHeader className="bg-primary-foreground/30">
         <TableRow>
-          <TableHead>Datum</TableHead>
-          <TableHead>Werknemers</TableHead>
-          <TableHead>Omschrijving</TableHead>
-          <TableHead>Uurloon</TableHead>
-          <TableHead>Uren</TableHead>
-          <TableHead>Bedrag excl. BTW</TableHead>
-          <TableHead>BTW</TableHead>
+          <TableHead className="text-blue-950">Datum</TableHead>
+          <TableHead className="text-blue-950">Werknemers</TableHead>
+          <TableHead className="text-blue-950">Omschrijving</TableHead>
+          <TableHead className="text-blue-950">Uurloon</TableHead>
+          <TableHead className="text-blue-950">Uren</TableHead>
+          <TableHead className="text-blue-950">Bedrag excl. BTW</TableHead>
+          <TableHead className="text-blue-950">BTW</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
+        <TableRow className="bg-gray-100">
           <TableCell>12-Okt-2024</TableCell>
           <TableCell>2</TableCell>
           <TableCell>ProjectNaam: FunctieNaam</TableCell>
           <TableCell>
-            <Euro />
+            <Euro className="relative -top-0.5 inline size-3" />
             12,30
           </TableCell>
           <TableCell>10.00</TableCell>
-          <TableCell>
-            <Euro />
+          <TableCell className="min-w-36">
+            <Euro className="relative -top-0.5 inline size-3" />
             147,43
           </TableCell>
           <TableCell>21%</TableCell>
