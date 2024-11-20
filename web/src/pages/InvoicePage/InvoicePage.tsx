fd2662ad879198a112be23862be9f3bbe34d4d1d
Jazelle Carillo <jazellemaira@pm.me>
2024-11-20 15:12:12 +0100

    fix(list-layout): Adjust elements positioning

--- web/src/pages/InvoicePage/InvoicePage.tsx
+++ web/src/pages/InvoicePage/InvoicePage.tsx
@@ -12,10 +12,16 @@ const InvoicePage = () => {
       <div className="mx-auto min-h-[120vh] bg-white">
         <div className="mx-auto flex max-w-4xl flex-col">
           <ul className="flex w-full flex-col items-center gap-1 pb-32 pt-20">
+            <li className="flex w-11/12 flex-wrap justify-end gap-2 rounded-md bg-primary/20 p-2 text-primary/70 xs:justify-between">
+              <div className="flex flex-wrap items-center gap-4">
+                <div className="flex items-center gap-1">
+                  <FileText />
+                  <span className="font-semibold">12 Okt 2024</span>
+                </div>
                 <span>Mollie Baker</span>
                 <Badge className="h-fit">Ongebetald</Badge>
               </div>
-              <div className="flex gap-4">
+              <div className="flex gap-4 px-2">
                 <Button className="bg-primary p-0">
                   <FileSearch />
                 </Button>
