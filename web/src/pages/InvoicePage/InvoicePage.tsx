5b7aba7e63830cb052bc8b99020dfd8f13595a04
Jazelle Carillo <jazellemaira@pm.me>
2024-11-20 15:10:35 +0100

    feat(invoice-info): Show woker's name and paid/unpaid badge

--- web/src/pages/InvoicePage/InvoicePage.tsx
+++ web/src/pages/InvoicePage/InvoicePage.tsx
@@ -3,6 +3,8 @@ import { Button } from 'web/src/components/ui/button'
 
 import { Metadata } from '@redwoodjs/web'
 
+import { Badge } from 'src/components/ui/badge'
+
 const InvoicePage = () => {
   return (
     <>
@@ -10,10 +12,8 @@ const InvoicePage = () => {
       <div className="mx-auto min-h-[120vh] bg-white">
         <div className="mx-auto flex max-w-4xl flex-col">
           <ul className="flex w-full flex-col items-center gap-1 pb-32 pt-20">
-            <li className="flex h-12 w-11/12 items-center justify-between rounded-md bg-primary/20 px-5 text-primary/70 xs:w-4/5">
-              <div className="flex w-3/4 gap-1">
-                <FileText />
-                <span className="font-semibold">12 Okt 2024</span>
+                <span>Mollie Baker</span>
+                <Badge className="h-fit">Ongebetald</Badge>
               </div>
               <div className="flex gap-4">
                 <Button className="bg-primary p-0">
