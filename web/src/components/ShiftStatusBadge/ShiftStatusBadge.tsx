173989eea6522354efe8100afbec67f753b03b4d
Jazelle Carillo <jazellemaira@pm.me>
2024-11-20 19:42:57 +0100

    feat(ShiftStatusBadge): Render a badge indicating the shift status

--- web/src/components/ShiftStatusBadge/ShiftStatusBadge.tsx
+++ web/src/components/ShiftStatusBadge/ShiftStatusBadge.tsx
@@ -1,13 +1,25 @@
-const ShiftStatusBadge = () => {
+import { WorkRequestsTodayQuery } from 'types/graphql'
+
+import { Badge } from 'src/components/ui/badge'
+
+type ShiftStatusBadgeProps = {
+  shift: WorkRequestsTodayQuery['workRequestsToday'][0]['shifts'][0]
+}
+
+const ShiftStatusBadge = ({ shift }: ShiftStatusBadgeProps) => {
   return (
-    <div>
-      <h2>{'ShiftStatusBadge'}</h2>
-      <p>
-        {
-          'Find me in ./web/src/components/ShiftStatusBadge/ShiftStatusBadge.tsx'
-        }
-      </p>
-    </div>
+    <>
+      {shift.status === 'CHECKED_IN' && (
+        <Badge variant="outline" className=" h-4 bg-lime-500 text-black">
+          In
+        </Badge>
+      )}
+      {shift.status === 'CHECKED_OUT' && (
+        <Badge variant="outline" className=" h-4 bg-red-500 text-black">
+          Uit
+        </Badge>
+      )}
+    </>
   )
 }
 
