983b4caab31f81fd4859df7be0924531ef7a7a0b
Jazelle Carillo <jazellemaira@pm.me>
2024-11-20 12:21:55 +0100

    feat(InvoiceDetails): Generate invoice details table

---
+++ web/src/components/InvoiceDetailsTable/InvoiceDetailsTable.tsx
@@ -0,0 +1,14 @@
+const InvoiceDetailsTable = () => {
+  return (
+    <div>
+      <h2>{"InvoiceDetailsTable"}</h2>
+      <p>
+        {
+          "Find me in ./web/src/components/InvoiceDetailsTable/InvoiceDetailsTable.tsx"
+        }
+      </p>
+    </div>
+  );
+};
+
+export default InvoiceDetailsTable;
