12938fa926720e288d2250440bba8c8fdc4d68a9
Jazelle Carillo <jazellemaira@pm.me>
2024-11-20 19:18:29 +0100

    feat(ShiftStatusBadge): Generate shift status badge component

---
+++ web/src/components/ShiftStatusBadge/ShiftStatusBadge.tsx
@@ -0,0 +1,14 @@
+const ShiftStatusBadge = () => {
+  return (
+    <div>
+      <h2>{'ShiftStatusBadge'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/ShiftStatusBadge/ShiftStatusBadge.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default ShiftStatusBadge
