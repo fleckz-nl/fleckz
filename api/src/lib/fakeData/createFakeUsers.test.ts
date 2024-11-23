6a11296371cef4c0d834793b1d91d00b46ffa04c
Nami Sunami <nsunami@pm.me>
2024-11-23 17:46:11 +0100

    feat(test-fakeUsers): Add additional tests & check lengths

--- api/src/lib/fakeData/createFakeUsers.test.ts
+++ api/src/lib/fakeData/createFakeUsers.test.ts
@@ -26,14 +26,23 @@ describe('createFakeUsers', () => {
 
   it('creates users with client roles by default', () => {
     const users = createFakeUsers()
-
+    expect(users).toHaveLength(5)
     users.forEach((user) => {
       expect(user).toHaveProperty('roles', ['CLIENT'])
     })
   })
 
+  it('creates users with temp agency representative roles', () => {
+    const tempAgencyReps = createFakeUsers({ roles: ['TEMP_AGENCY_REP'] })
+    expect(tempAgencyReps).toHaveLength(5)
+    tempAgencyReps.forEach((user) => {
+      expect(user).toHaveProperty('roles', ['TEMP_AGENCY_REP'])
+    })
+  })
+
   it('creates users with admin roles', () => {
     const adminUsers = createFakeUsers({ roles: ['ADMIN'] })
+    expect(adminUsers).toHaveLength(5)
     adminUsers.forEach((user) => {
       expect(user).toHaveProperty('roles', ['ADMIN'])
     })
