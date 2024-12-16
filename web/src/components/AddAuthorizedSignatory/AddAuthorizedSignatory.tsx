bf1c07f1c109c429b006441f50b6850b87b1fceb
Jazelle Carillo <jazellemaira@pm.me>
2024-12-16 16:46:35 +0100

    feat(onboarding): Generate AddAuthorizedSignatory component

---
+++ web/src/components/AddAuthorizedSignatory/AddAuthorizedSignatory.tsx
@@ -0,0 +1,14 @@
+const AddAuthorizedSignatory = () => {
+  return (
+    <div>
+      <h2>{'AddAuthorizedSignatory'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/AddAuthorizedSignatory/AddAuthorizedSignatory.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default AddAuthorizedSignatory
