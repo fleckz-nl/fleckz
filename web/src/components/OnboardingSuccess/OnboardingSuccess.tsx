75f1fe77ca0f5b9d3ae34b2b1a9747b8225c482b
Nami Sunami <nsunami@pm.me>
2024-12-21 21:10:27 +0100

    feat(OnboardingSuccess): Generate component

---
+++ web/src/components/OnboardingSuccess/OnboardingSuccess.tsx
@@ -0,0 +1,14 @@
+const OnboardingSuccess = () => {
+  return (
+    <div>
+      <h2>{'OnboardingSuccess'}</h2>
+      <p>
+        {
+          'Find me in ./web/src/components/OnboardingSuccess/OnboardingSuccess.tsx'
+        }
+      </p>
+    </div>
+  )
+}
+
+export default OnboardingSuccess
