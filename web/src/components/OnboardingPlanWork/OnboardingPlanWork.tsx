5a73bf5fb075c332dbca70c76db8b87de31db4e3
Nami Sunami <nsunami@pm.me>
2025-01-02 16:53:42 +0100

    feat(OnboardingPlanWork): Add the next button

--- web/src/components/OnboardingPlanWork/OnboardingPlanWork.tsx
+++ web/src/components/OnboardingPlanWork/OnboardingPlanWork.tsx
@@ -1,5 +1,7 @@
 import { ArrowLeft } from 'lucide-react'
 
+import { Button } from 'src/components/ui/button'
+import WorkSchedularCell from 'src/components/WorkSchedularCell'
 import { OnboardingStages } from 'src/pages/OnboardingPage/OnboardingPage'
 type OnboardingPlanWorkProps = {
   setOnboardingStep: React.Dispatch<React.SetStateAction<OnboardingStages>>
@@ -8,13 +10,26 @@ const OnboardingPlanWork = ({ setOnboardingStep }: OnboardingPlanWorkProps) => {
   function handlePreviousClick() {
     setOnboardingStep('addJobProfile')
   }
+  function handleNextClick() {
+    // setOnboardingStep('')
+  }
   return (
-    <div className="mx-auto mt-8 flex max-w-xl flex-col gap-6 text-white">
+    <div className="mx-auto my-8 flex max-w-xl flex-col gap-6 text-white">
       <ArrowLeft className="cursor-pointer" onClick={handlePreviousClick} />
       <h1 className="text-2xl font-bold text-white">
         Wanneer zijn er medewerkers nodig?
       </h1>
       <p>Klik in de agenda of klik op werk plaatsen.</p>
+      <div className="mb-16">
+        <WorkSchedularCell />
+      </div>
+      <Button
+        className="self-end bg-secondary py-4 text-lg"
+        type="submit"
+        onClick={handleNextClick}
+      >
+        Volgende
+      </Button>
     </div>
   )
 }
