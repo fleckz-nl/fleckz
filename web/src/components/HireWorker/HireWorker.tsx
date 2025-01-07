efd95f8d6e60f595fe48934574e3656f1ac91081
Nami Sunami <nsunami@pm.me>
2025-01-07 20:46:44 +0100

    feat(HireWorker): Implement the confirmation pending view

--- web/src/components/HireWorker/HireWorker.tsx
+++ web/src/components/HireWorker/HireWorker.tsx
@@ -1,4 +1,6 @@
-import { ArrowLeft, Filter } from 'lucide-react'
+import { useState } from 'react'
+
+import { ArrowLeft, Filter, UserRound, UserRoundCheck } from 'lucide-react'
 
 import { Avatar, AvatarFallback, AvatarImage } from 'src/components/ui/avatar'
 import { Button } from 'src/components/ui/button'
@@ -11,11 +13,16 @@ type HireWorkerProps = {
 }
 
 const HireWorker = ({ setOnboardingStep }: HireWorkerProps) => {
+  const [stage, setStage] = useState<
+    'selectWorker' | 'pendingConfirmation' | 'Confirmed'
+  >('selectWorker')
   function handlePreviousClick() {
     setOnboardingStep('planWork')
   }
   function handleNextClick() {
-    // setOnboardingStep('')
+    setStage((currentStage) =>
+      currentStage === 'selectWorker' ? 'pendingConfirmation' : 'Confirmed'
+    )
   }
 
   return (
@@ -28,19 +35,8 @@ const HireWorker = ({ setOnboardingStep }: HireWorkerProps) => {
         Hieronder staan de kandidaten die beschikbaar zijn op het geselecteerde
         profiel en de geselecteerde datum en tijd.
       </p>
-      <div className="mb-16 space-y-6">
-        <div>Functieprofiel: Afwasser</div>
-        <div>
-          <Filter className="mr-2 inline" />
-          Filteren
-        </div>
-        {workers.map((worker) => (
-          <>
-            <WorkerProfile key={worker.id} worker={worker} />
-            <Separator />
-          </>
-        ))}
-      </div>
+      {stage === 'selectWorker' && <SelectWorker />}
+      {stage === 'pendingConfirmation' && <PendingConfirmation />}
       <Button
         className="self-end bg-secondary py-4 text-lg"
         type="submit"
@@ -48,6 +44,50 @@ const HireWorker = ({ setOnboardingStep }: HireWorkerProps) => {
       >
         Volgende
       </Button>
+    </div>
+  )
+}
+
+const SelectWorker = () => {
+  return (
+    <div className="mb-16 space-y-6">
+      <div>Functieprofiel: Afwasser</div>
+      <div>
+        <Filter className="mr-2 inline" />
+        Filteren
+      </div>
+      {workers.map((worker) => (
+        <>
+          <WorkerProfile key={worker.id} worker={worker} />
+          <Separator />
+        </>
+      ))}
+    </div>
+  )
+}
+
+const PendingConfirmation = () => {
+  const percentConfirmed =
+    (workers.filter((worker) => worker.status === 'confirmed').length /
+      workers.length) *
+    100
+  return (
+    <div className="mb-16 space-y-6">
+      <div className="mx-4 mb-8 flex items-center">
+        <div className="mr-4">Status</div>
+        <div className="w-full border p-1">
+          <div
+            className="h-7 bg-secondary"
+            style={{ width: percentConfirmed + '%' }}
+          ></div>
+        </div>
+      </div>
+      {workers.map((worker) => (
+        <>
+          <WorkerConfirmationStatus key={worker.id} worker={worker} />
+          <Separator />
+        </>
+      ))}
     </div>
   )
 }
@@ -120,7 +160,69 @@ const WorkerProfile = ({
   )
 }
 
-const workers = [
+type WorkerConfirmationStatusProps = {
+  worker: Worker
+}
+
+const WorkerConfirmationStatus = ({
+  worker: { avatarId, hourlyWage, tempAgency, status },
+}: WorkerConfirmationStatusProps) => {
+  return (
+    <div>
+      <div className="flex space-x-4">
+        <Avatar className="size-24">
+          <AvatarImage
+            src={`https://avatar.iran.liara.run/public/${avatarId}`}
+          />
+          <AvatarFallback>
+            <img
+              src={`https://avatar.iran.liara.run/public/${avatarId}`}
+              alt=""
+            />
+          </AvatarFallback>
+        </Avatar>
+        <div className="flex w-full flex-row items-center">
+          <div className="mx-auto flex-col text-nowrap">
+            <div className="font-bold">{tempAgency.name}</div>
+            <div>
+              {(status === 'pending' && (
+                <span className="text-red-400">Niet bevestigd</span>
+              )) ||
+                (status === 'confirmed' && (
+                  <span className="text-green-300">Bevestigd</span>
+                ))}
+            </div>
+          </div>
+          <div className="ml-auto mr-6 flex items-center justify-center rounded-full border border-secondary p-4 *:size-11">
+            {status === 'pending' && <UserRound className="text-red-400" />}
+            {status === 'confirmed' && (
+              <UserRoundCheck className="text-green-400" />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="mt-2 flex justify-between">
+        <div className="ml-4">{formatToEuros(hourlyWage)}</div>
+      </div>
+    </div>
+  )
+}
+
+type Worker = {
+  id: number
+  avatarId: number
+  softSkills: number
+  hardSkills: number
+  brutoloon: number
+  hourlyWage: number
+  tempAgency: {
+    id: number
+    name: string
+  }
+  status: 'pending' | 'confirmed'
+}
+
+const workers: Worker[] = [
   {
     id: 1,
     avatarId: 5,
@@ -128,6 +230,11 @@ const workers = [
     hardSkills: 1,
     brutoloon: 0.5,
     hourlyWage: 27,
+    tempAgency: {
+      id: 1,
+      name: 'Flexi Work B.V.',
+    },
+    status: 'pending',
   },
   {
     id: 2,
@@ -136,6 +243,11 @@ const workers = [
     hardSkills: 0.7,
     brutoloon: 0.6,
     hourlyWage: 30,
+    tempAgency: {
+      id: 2,
+      name: 'TempMasters B.V.',
+    },
+    status: 'confirmed',
   },
   {
     id: 3,
@@ -144,6 +256,11 @@ const workers = [
     hardSkills: 0.9,
     brutoloon: 0.7,
     hourlyWage: 33,
+    tempAgency: {
+      id: 3,
+      name: 'TempoGroup B.V.',
+    },
+    status: 'confirmed',
   },
 ]
 
