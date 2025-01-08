4df6e8571e60fc4ff017c27000de834d28c74039
Jazelle Carillo <jazellemaira@pm.me>
2025-01-08 18:29:10 +0100

    feat(SelectRole): Replace image for better cohesion with other pages

--- web/src/components/OnboardingSelectRole/OnboardingSelectRole.tsx
+++ web/src/components/OnboardingSelectRole/OnboardingSelectRole.tsx
@@ -16,11 +16,16 @@ const OnboardingSelectRole = ({
     <>
       <header className="center mx-auto mt-8 flex max-w-md flex-col">
         <img
-          src={workersMarch}
-          alt="Workers marching forward"
-          className="max-h-60 p-4"
+          src="images/team-leader-teamwork-concept_9176886_6671.svg"
+          alt="A team leader holding a telescope with four coworkers"
+          className="-mb-8 self-center pb-4"
         />
-        <h3 className="font-semibold">Wat wil je doen?</h3>
+        <span className="text-right text-xs text-gray-500">
+          <a href="http://www.freepik.com">
+            Image designed by pch.vector / Freepik
+          </a>
+        </span>
+        <h3 className="mt-8 text-xl font-semibold">Wat wil je doen?</h3>
       </header>
       <div className="mx-auto mt-8 flex max-w-md flex-col gap-2">
         <Button
