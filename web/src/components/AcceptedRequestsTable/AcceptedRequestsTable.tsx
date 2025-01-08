ae96d75d7b0fd1a24fc6d2247ef818249fc45e08
Jazelle Carillo <jazellemaira@pm.me>
2025-01-08 13:55:55 +0100

    feat(OverviewPage): Remove requests table. Add sort and search components

--- web/src/components/AcceptedRequestsTable/AcceptedRequestsTable.tsx
+++ web/src/components/AcceptedRequestsTable/AcceptedRequestsTable.tsx
@@ -1,19 +1,13 @@
 import { format } from 'date-fns/format'
 import { nl } from 'date-fns/locale/nl'
-import { CheckCircle2, Users } from 'lucide-react'
+import { Users } from 'lucide-react'
 import { WorkRequestsQuery } from 'types/graphql'
 
 import { navigate } from '@redwoodjs/router'
 
 import { OverviewHeader } from 'src/components/OverviewSection'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from 'src/components/ui/table'
+import SearchInput from 'src/components/SearchInput/SearchInput'
+import SortButton from 'src/components/SortButton/SortButton'
 
 type AcceptedRequestsTableProps = {
   workRequests: WorkRequestsQuery['workRequests']
@@ -23,45 +17,12 @@ const AcceptedRequestsTable = ({
 }: AcceptedRequestsTableProps) => {
   const acceptedRequests = workRequests.filter((r) => r.status === 'CONFIRMED')
   return (
-    <div className="flex flex-col">
-      <OverviewHeader>
-        <CheckCircle2 className="mr-1 inline" />
-        Geaccepteerd
-      </OverviewHeader>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="text-gray-400">Datum</TableHead>
-            <TableHead className="text-gray-400">Functienaam</TableHead>
-            <TableHead className="text-gray-400">Werknemers</TableHead>
-            <TableHead className="text-gray-400">Werktijd</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {acceptedRequests.map((request) => (
-            <TableRow
-              key={request.id}
-              className="bg-primary text-white hover:cursor-pointer hover:bg-white hover:text-secondary"
-              onClick={() => navigate(`/requests/${request.id}`)}
-            >
-              <TableCell className="font-bold">
-                {format(request.startDate, 'd MMMM, yyyy', {
-                  locale: nl,
-                })}
-              </TableCell>
-              <TableCell>{request.jobProfile.name}</TableCell>
-              <TableCell className="pl-10">
-                {request.shifts.length}
-                <Users className="relative -top-0.5 ml-1 inline size-4" />
-              </TableCell>
-              <TableCell>
-                {format(request.startDate, 'H:MM')}–
-                {format(request.endDate, 'H:MM')}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+    <div className="flex flex-col gap-4 text-white">
+      <OverviewHeader>Werklijst</OverviewHeader>
+      <div className="flex justify-between gap-2">
+        <SortButton />
+        <SearchInput />
+      </div>
     </div>
   )
 }
