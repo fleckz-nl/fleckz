71c6d413e6c4f0c0a262b7dad48d1e143f210de8
Nami Sunami <nsunami@pm.me>
2025-01-11 15:15:46 +0100

    chore(Certificates): Generate the component

---
+++ web/src/components/Certificates/Certificates.tsx
@@ -0,0 +1,10 @@
+const Certificates = () => {
+  return (
+    <div>
+      <h2>{'Certificates'}</h2>
+      <p>{'Find me in ./web/src/components/Certificates/Certificates.tsx'}</p>
+    </div>
+  )
+}
+
+export default Certificates
