aa7cf950cb767ce124b708f75bd4e387ff869777
Nami Sunami <nsunami@pm.me>
2025-01-13 19:11:51 +0100

    chore(CvsList): Generate the component

---
+++ web/src/components/CvsList/CvsList.tsx
@@ -0,0 +1,10 @@
+const CvsList = () => {
+  return (
+    <div>
+      <h2>{'CvsList'}</h2>
+      <p>{'Find me in ./web/src/components/CvsList/CvsList.tsx'}</p>
+    </div>
+  )
+}
+
+export default CvsList
