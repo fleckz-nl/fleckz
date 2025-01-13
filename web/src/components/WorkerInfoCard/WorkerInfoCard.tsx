bc4d0e323724e7d51d17a6a377945922392c79cc
Jazelle Carillo <jazellemaira@pm.me>
2025-01-13 12:20:51 +0100

    chore(WorkerInfo): Generate the component

---
+++ web/src/components/WorkerInfoCard/WorkerInfoCard.tsx
@@ -0,0 +1,12 @@
+const WorkerInfoCard = () => {
+  return (
+    <div>
+      <h2>{'WorkerInfoCard'}</h2>
+      <p>
+        {'Find me in ./web/src/components/WorkerInfoCard/WorkerInfoCard.tsx'}
+      </p>
+    </div>
+  )
+}
+
+export default WorkerInfoCard
