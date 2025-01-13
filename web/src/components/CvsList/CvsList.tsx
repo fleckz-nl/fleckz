a8509da0c0f90086587a1c0a5f1d19b2f1cfe5c0
Nami Sunami <nsunami@pm.me>
2025-01-13 19:55:37 +0100

    feat(CvsList): Add filter functionality and navigation buttons

--- web/src/components/CvsList/CvsList.tsx
+++ web/src/components/CvsList/CvsList.tsx
@@ -1,9 +1,75 @@
+import { useContext } from 'react'
+
+import { ArrowLeft } from 'lucide-react'
+
+import { navigate, routes } from '@redwoodjs/router'
+
+import { Button } from 'src/components/ui/button'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from 'src/components/ui/select'
+import WorkerInfoCard from 'src/components/WorkerInfoCard/WorkerInfoCard'
+import { OnboardingContext } from 'src/pages/OnboardingPage/OnboardingContext'
+
 const CvsList = () => {
+  const { setOnboardingStep } = useContext(OnboardingContext)
+  function handlePreviousClick() {
+    setOnboardingStep('cvUpload')
+  }
+
+  function handleNextClick() {
+    navigate(routes.overview())
+  }
+
   return (
-    <div>
-      <h2>{'CvsList'}</h2>
-      <p>{'Find me in ./web/src/components/CvsList/CvsList.tsx'}</p>
+    <div className="mx-auto mt-8 flex max-w-xl flex-col gap-4">
+      <ArrowLeft className="cursor-pointer" onClick={handlePreviousClick} />
+      <h1 className="text-2xl font-bold text-white">CVs ingelezen</h1>
+      <div>
+        <Filter />
+      </div>
+      <WorkerInfoCard />
+      <Button
+        className="self-end bg-secondary py-4 text-lg"
+        type="submit"
+        onClick={handleNextClick}
+      >
+        Volgende
+      </Button>
     </div>
+  )
+}
+
+const Filter = () => {
+  const functionFilters = [
+    { value: 'afwasser', label: 'Afwasser' },
+    { value: 'keukenhulp', label: 'Keukenhulp' },
+    { value: 'kok', label: 'Kok' },
+  ]
+  return (
+    <Select>
+      <SelectTrigger className="w-[210px]">
+        <SelectValue placeholder="Filter" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Functie</SelectLabel>
+          <SelectSeparator />
+          {functionFilters.map((option) => (
+            <SelectItem key={option.value} value={option.value}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   )
 }
 
