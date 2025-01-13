5f8c01839f846b6dec432bfee03f85e34117dc2c
Nami Sunami <nsunami@pm.me>
2025-01-13 21:31:57 +0100

    feat(EditWorkerInfo):  Add worker information form

--- web/src/components/EditWorkerInfo/EditWorkerInfo.tsx
+++ web/src/components/EditWorkerInfo/EditWorkerInfo.tsx
@@ -1,10 +1,104 @@
-const EditWorkerInfo = () => {
+import { ArrowLeft } from 'lucide-react'
+
+import TextInput from 'src/components/TextInput/TextInput'
+import { Avatar, AvatarFallback, AvatarImage } from 'src/components/ui/avatar'
+import { Button } from 'src/components/ui/button'
+import { Label } from 'src/components/ui/label'
+import { SwitchWhite } from 'src/components/ui/switch'
+import { Skill } from 'src/components/WorkerInfoCard/WorkerInfoCard'
+
+type EditWorkerInfoProps = {
+  worker: {
+    id: number
+    name: string
+    jobFunction: string
+    yearOfExp: number
+    age: number
+    hasCar: boolean
+    livingPlace: string
+    factor: number
+    softSkills: Skill[]
+  }
+  setEditOpen: (open: boolean) => void
+}
+
+const EditWorkerInfo = ({ worker, setEditOpen }: EditWorkerInfoProps) => {
+  const { id } = worker
+
+  function handlePreviousClick() {
+    setEditOpen(false)
+  }
+
+  function handleNextClick() {
+    setEditOpen(false)
+  }
   return (
-    <div>
-      <h2>{'EditWorkerInfo'}</h2>
-      <p>
-        {'Find me in ./web/src/components/EditWorkerInfo/EditWorkerInfo.tsx'}
-      </p>
+    <div className="mx-auto mt-8 flex max-w-xl flex-col gap-4 px-4">
+      <ArrowLeft
+        className="cursor-pointer text-white"
+        onClick={handlePreviousClick}
+      />
+      <h1 className="text-2xl font-bold text-white">Gegevens aanpassen</h1>
+      <Avatar className="size-16">
+        <AvatarImage src={`https://avatar.iran.liara.run/public/${id}`} />
+        <AvatarFallback>
+          <img
+            src={`https://avatar.iran.liara.run/public/${id}`}
+            alt="Random avatar"
+          />
+        </AvatarFallback>
+      </Avatar>
+      <div>
+        <Label htmlFor="first-name">Voornaam</Label>
+        <TextInput id="first-name" className="align-middle" />
+      </div>
+      <div>
+        <Label htmlFor="last-name">Achternaam</Label>
+        <TextInput id="last-name" className="align-middle" />
+      </div>
+      <div>
+        <Label htmlFor="phone">Telefoon</Label>
+        <TextInput id="phone" className="align-middle" />
+      </div>
+      <div>
+        <Label htmlFor="age">Leeftijd</Label>
+        <TextInput id="age" className="align-middle" />
+      </div>
+      <div>
+        <Label htmlFor="living-place">Woonplaats</Label>
+        <TextInput id="living-place" className="align-middle" />
+      </div>
+      <div className="flex items-center justify-between">
+        <Label className="mr-8 text-xl" htmlFor={'drivingLicense'}>
+          Rijbewijs
+        </Label>
+        <SwitchWhite id="drivingLicense" />
+      </div>
+      <div className="flex items-center justify-between">
+        <Label className="mr-8 text-xl" htmlFor={'ownTransport'}>
+          Eigen vervoer
+        </Label>
+        <SwitchWhite id="ownTransport" />
+      </div>
+      <div className="flex items-center justify-between">
+        <Label className="mr-8 text-xl" htmlFor={'chronologicalExperience'}>
+          Chronologisch werkervaring
+        </Label>
+        <SwitchWhite id="chronologicalExperience" />
+      </div>
+      <div className="flex items-center justify-between">
+        <Label className="mr-8 text-xl" htmlFor={'noTimeGaps'}>
+          Geen tijdsgaten
+        </Label>
+        <SwitchWhite id="noTimeGaps" />
+      </div>
+      <Button
+        className="self-end bg-secondary py-4 text-lg"
+        type="submit"
+        onClick={handleNextClick}
+      >
+        Volgende
+      </Button>
     </div>
   )
 }
