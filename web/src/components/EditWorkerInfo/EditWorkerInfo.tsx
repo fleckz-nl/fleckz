102648304cfa4658e4ac55b920889ea89b7c960e
Nami Sunami <nsunami@pm.me>
2025-01-13 17:06:24 +0100

    chore(EditWorkerInfo): Generate the component

---
+++ web/src/components/EditWorkerInfo/EditWorkerInfo.tsx
@@ -0,0 +1,12 @@
+const EditWorkerInfo = () => {
+  return (
+    <div>
+      <h2>{'EditWorkerInfo'}</h2>
+      <p>
+        {'Find me in ./web/src/components/EditWorkerInfo/EditWorkerInfo.tsx'}
+      </p>
+    </div>
+  )
+}
+
+export default EditWorkerInfo
