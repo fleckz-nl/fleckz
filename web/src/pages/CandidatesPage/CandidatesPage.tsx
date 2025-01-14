4caf37cea281dcd44f0bfed47384799e1f8f0977
Nami Sunami <nsunami@pm.me>
2025-01-14 20:28:14 +0100

    feat(CandidatesPage): Update CandidatesPage with filter and search fields

--- web/src/pages/CandidatesPage/CandidatesPage.tsx
+++ web/src/pages/CandidatesPage/CandidatesPage.tsx
@@ -1,20 +1,33 @@
 // import { Link, routes } from '@redwoodjs/router'
+import { useState } from 'react'
+
 import { Metadata } from '@redwoodjs/web'
 
+import { Filter, workers } from 'src/components/CvsList/CvsList'
+import SearchInput from 'src/components/SearchInput'
+import WorkerInfoCard from 'src/components/WorkerInfoCard/WorkerInfoCard'
+
 const CandidatesPage = () => {
+  const [searchValue, setSearchValue] = useState('')
   return (
     <>
       <Metadata title="Candidates" description="Candidates page" />
 
-      <h1>CandidatesPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/CandidatesPage/CandidatesPage.tsx</code>
-      </p>
-      {/*
-          My default route is named `candidates`, link to me with:
-          `<Link to={routes.candidates()}>Candidates</Link>`
-      */}
+      <div className="mx-auto mt-8 flex max-w-xl flex-col gap-4">
+        <h1 className="text-2xl font-bold text-white">Kandidaten</h1>
+        <div className="flex justify-between">
+          <Filter />
+          <SearchInput
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+        <div className="mb-8 space-y-8">
+          {workers.map((worker) => (
+            <WorkerInfoCard key={worker.id} worker={worker} />
+          ))}
+        </div>
+      </div>
     </>
   )
 }
